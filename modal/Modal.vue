288a6610f8b80521a585a39a3cc0aca96564e931
Jasper Tey <jasper.tey@gmail.com>
2021-01-12 09:38:14 -0500

    Adding the very early version of the vue equivalent of the classic io modal, for reuse in other projects.

---
+++ modal/Modal.vue
@@ -0,0 +1,155 @@
+<template>
+	<div class="io-modal" :id="id">
+		<div class="io-modal-toolbar clearfix">
+			<a class="btn btn-primary btn-sm" href="#" @click="close" v-if="modals.length > 1" style="display: none;">
+				<i class="fa fa-chevron-left"></i>
+			</a>
+			<a class="btn btn-default btn-sm float-right pull-right" href="#" @click="close" ref="closeButton">
+				<i class="fa fa-times"></i>
+			</a>
+		</div>
+		<div class="io-modal-content" ref="modalContent">
+			<component :is="componentLoaded" :params="componentParams"></component>
+		</div>
+	</div>
+</template>
+
+<script>
+import $ from 'jquery';
+import _ from 'lodash';
+import App from 'App';
+import {unref, defineAsyncComponent} from 'vue';
+
+var $body = $('body');
+var $main = $('.io-viewport').first();
+
+var loaded = {};
+
+export default {
+	name: "Modal",
+	props: {
+		data: Object
+	},
+	inject: ['util', 'scales', 'parent', 'modals', 'data'],
+	data: function () {
+		return {
+			id: null,
+			token: '',
+			size: 'lg',
+			visible: false,
+			componentLoaded: null,
+			componentParams: {}
+		};
+	},
+	mounted() {
+		var $modal = $(this.$el);
+		this.init();
+		/*console.log('Modal mounted!', this);
+		$modal.modal({
+			backdrop: true,
+			focus: true,
+			show: true
+		});*/
+	},
+	unmounted() {
+		this.dispose();
+	},
+	methods: {
+		init: function () {
+			this.id = _.uniqueId('modal-');
+			this.token = Date.now();
+
+			this.util.freezeBody();
+
+			window.addEventListener('resize', this.resize);
+
+			if (this.data.component) {
+				this.loadComponent(this.data.component, this.data.data);
+			}
+		},
+		dispose: function () {
+			window.removeEventListener('resize', this.resize);
+		},
+		close: function () {
+			var container = this.parent;
+			this.dispose();
+			container.close();
+			return false;
+		},
+		loadComponent: function (name, data) {
+			var componentName = `injected-component-${name}`;
+
+			App.vue.component(componentName, defineAsyncComponent(
+			  () => import(`components/app/${name}.vue`)
+			));
+
+			this.componentLoaded = componentName;
+			this.componentParams = data;
+
+			this.resize();
+		},
+		resize: _.throttle(function () {
+			var $modalContainer = $(this.$el);
+			var $modalBody = $(this.$refs.modalContent);
+
+			//console.log('Resize', this.token, this.id);
+
+			var W = $main.width();
+			var H = window.innerHeight
+			  || document.documentElement.clientHeight
+			  || document.body.clientHeight;
+
+			var windowW = window.innerWidth
+			  || document.documentElement.clientWidth
+			  || document.body.clientWidth;
+
+			var gutter = 50;
+			var offset = $main.offset();
+			var size = this.size;
+
+			if (W < 900) {
+				// Force full-size when window is sufficiently small
+				size = 'lg';
+				gutter = 20;
+			}
+
+			if (W < 600) {
+				offset.left = 0;
+				W = $body.width();
+			}
+
+			if (W < 450) {
+				gutter = 10;
+			}
+
+			var scale = this.scales[size];
+
+			var modalW = Math.floor(scale.p * W) - gutter;
+
+			if (H > 680) {
+				var modalH = Math.floor(scale.p * H) - gutter;
+			} else {
+				var modalH = Math.floor(H - gutter);
+			}
+
+			$modalContainer.css({
+				width: modalW,
+				height: modalH,
+				top: 0 + (gutter / 2),
+				left: offset.left + Math.floor((W - modalW) / 2),
+				marginBottom: '50px'
+			});
+
+			var containerHeight = $modalContainer.height() - 50;
+			$modalBody.css({
+				overflowX: 'hidden'
+			});
+			if (!$modalBody.hasClass('locked')) {
+				$modalBody.outerHeight(containerHeight);
+			}
+
+			return false;
+		}, 100)
+	}
+}
+</script>
---
+++ modal/ModalContainer.vue
@@ -0,0 +1,226 @@
+<template>
+	<teleport to="#externals">
+		<div id="ix-modals">
+			<div class="io-modal-mask" v-if="data.items.length > 0"></div>
+			<div ref="modals">
+				<modal v-for="(item, index) in data.items"
+				  :data="item"
+				  :index="index"
+				></modal>
+			</div>
+		</div>
+	</teleport>
+</template>
+
+<script>
+import $ from 'jquery';
+import _ from 'lodash';
+import Modal from './Modal.vue';
+import {provide, reactive, ref, unref, watch} from 'vue';
+import Url from 'url-parse';
+
+/*import Vue from 'vue';
+var ModalClass = Vue.extend(Modal);*/
+
+var $body = $('body');
+var $main = $('.io-viewport').first();
+var $mask = $('<div class="io-modal-mask"></div>');
+var scrollPosition = null;
+
+if ($main.length == 0) {
+	$main = $body;
+}
+
+var scales = {
+	sm: {
+		p: 0.5
+	},
+	md: {
+		p: 0.7
+	},
+	lg: {
+		p: 1
+	}
+};
+
+var util = {
+	freezeBody: function () {
+		if (scrollPosition === null) {
+			scrollPosition = $(window).scrollTop();
+		}
+		$body.css({
+			overflow: 'hidden',
+			position: 'fixed',
+			width: '100%'
+		});
+	},
+	restoreBody: function () {
+		$body.css({
+			overflow: 'auto',
+			position: 'static',
+			width: 'auto'
+		});
+		$(window).scrollTop(scrollPosition);
+		scrollPosition = null;
+	}
+};
+
+export default {
+	name: "Modals",
+	components: {
+		Modal
+	},
+	/*data: function () {
+		return {
+			visible: false,
+			items: []
+		};
+	},*/
+	setup() {
+		const data = reactive({
+			items: []
+		});
+
+		//console.log('setup() modals', unref(data));
+		provide('modals', data.items);
+		provide('data', data);
+
+		/*watch(data, (data, oldData) => {
+			console.log('WATCH data.items', data.items);
+		});*/
+
+		return {
+			data
+		}
+	},
+	provide() {
+		return {
+			util: util,
+			scales: scales,
+			parent: this,
+			modals: this.data.items
+		}
+	},
+	mounted() {
+		this.init();
+		window.$Modal = this;
+	},
+	methods: {
+		init: _.once(function () {
+			var $template = $(this.$el);
+
+			$mask = $template.find('.io-modal-mask');
+			$mask.click(this.close);
+
+			var _this = this;
+
+			$(document).on('click', 'a[data-component-modal]', function (e) {
+				e.preventDefault();
+				var $this = $(this);
+				var path = $this.attr('data-component-modal');
+				var url = Url(`/${path}`, true);
+
+				var componentName = url.pathname.replace(/^\/+/, '');
+
+				//console.log(componentName, url);
+
+				_this.loadComponent(componentName, url.query);
+				return false;
+			});
+		}),
+		close: function () {
+			this.data.items = [];
+
+			if (this.data.items.length == 0) {
+				util.restoreBody();
+			}
+		},
+		loadComponent: function (name, data) {
+			var c = {
+				component: name,
+				data: data
+			};
+			var modals = this.data.items;
+			modals.push(c);
+		}
+	},
+	computed: {
+		nItems() {
+			//console.log('nItems', this.data.items.length);
+			return this.data.items.length;
+		}
+	}
+}
+</script>
+
+<style type="text/css">
+.io-modal {
+	position: fixed;
+	margin: 0 auto;
+	width: 400px;
+	border: 1px solid #ddd;
+	z-index: 9000;
+	background: #fcfcfc;
+	box-shadow: 0 3px 9px rgba(0, 0, 0, 0.5);
+}
+
+.io-modal .modal-hidden {
+	display: none;
+}
+
+.io-modal-mask {
+	z-index: 8999;
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background: #000;
+	opacity: 0.5;
+	filter: alpha(opacity=50);
+}
+
+.io-modal .io-modal-content {
+	display: block;
+	padding: 10px;
+	overflow-y: auto;
+}
+
+.io-modal .block hr {
+	margin: 20px -10px;
+}
+
+.io-modal-toolbar {
+	padding: 5px;
+	background: #efefef;
+}
+
+.io-modal .io-modal-content .content-header {
+	border-top: none;
+	margin-bottom: 20px;
+	margin-left: -10px;
+	margin-top: -10px;
+}
+
+.io-modal .block {
+	border: 1px solid #eee;
+}
+
+.io-modal ul.breadcrumb {
+	display: none;
+}
+
+.io-modal .selectize-dropdown {
+	z-index: 9000;
+}
+
+.modal,
+.modal-dialog,
+.bootstrap-dialog {
+	z-index: 9100 !important;
+}
+
+.modal-backdrop {
+	z-index: 1020 !important;
+}
+</style>

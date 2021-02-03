c04ec3f96363b595ed768b64098cfc49229d498a
Jasper Tey <jasper.tey@gmail.com>
2021-02-03 02:18:18 -0500

    Implementing Autosize.js directive for Vue3; and removing the previously prototyped Vue2 version.

---
+++ directives/autosize.js
@@ -0,0 +1,23 @@
+import $ from 'jquery';
+import autosize from 'autosize';
+
+export default {
+	name: 'autosize',
+	beforeMount: function (el, binding, vnode) {
+		var $el = $(el);
+
+		autosize($el);
+
+		$el.change(function () {
+			autosize.update(el);
+		});
+
+		$el.trigger('change');
+	},
+	updated: function (el) {
+		autosize.update(el);
+	},
+	unmounted: function (el) {
+		autosize.destroy($(el));
+	}
+}

f42844efd48b8f9b0d87c7f1fff375ea769f6425
Jasper Tey <jasper.tey@gmail.com>
2021-02-23 09:15:05 -0500

    OptionsMulti wrapper component of the Multiselect component, a starting point for the more advanced Select/Dropdown use cases.

---
+++ src/components/form/OptionsMulti.vue
@@ -0,0 +1,41 @@
+<template>
+	<multi-select
+	  v-model="modelValue"
+	  @change="$emit('update:modelValue', $event)"
+	>
+		<slot></slot>
+	</multi-select>
+</template>
+
+<script>
+import MultiSelect from '@vueform/multiselect';
+
+import "@vueform/multiselect/themes/default.css";
+
+export default {
+	name: "OptionsMulti",
+	inheritAttrs: true,
+	components: {
+		MultiSelect
+	},
+	emits: ['update:modelValue'],
+	props: {
+		modelValue: String,
+	},
+	setup(props) {
+		return {
+		};
+	},
+	computed: {
+	},
+	methods: {},
+	mounted() {
+	},
+	unmounted() {
+	}
+}
+</script>
+
+<style scoped>
+
+</style>

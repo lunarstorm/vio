e653f62282c72ace2ae21a6ac6191d11c5c1e051
Jasper Tey <jasper.tey@gmail.com>
2021-03-02 08:57:14 -0500

    Stashing the old modals for the timebeing.

---
+++ src/modal/tmp/ModalContainerOld.vue
@@ -0,0 +1,226 @@
+<template>
+	<teleport to="#externals">
+		<div id="ix-modals">
+			<div class="io-modal-mask" v-if="data.items.length > 0"></div>
+			<div ref="modals">
+				<modal v-for="(item, index) in data.items"
+				  :data="item"
+				  :index="index"
+				  :key="index"
+				  v-bind="item.props"
+				  @closed="removeItem(index)"
+				></modal>
+			</div>
+		</div>
+	</teleport>
+</template>
+
+<script>
+import $ from 'jquery';
+import _ from 'lodash';
+import Ui from 'vio/Ui';
+import Modal from './Modal.vue';
+import {provide, reactive, ref, unref, watch} from 'vue';
+import Url from 'url-parse';
+
+var $body = $('body');
+var $main = $('.io-viewport').first();
+var $mask = $('<div class="io-modal-mask"></div>');
+var scrollPosition = null;
+
+if ($main.length == 0) {
+	$main = $body;
+}
+
+var scales = {
+	sm: {
+		p: 0.5
+	},
+	md: {
+		p: 0.7
+	},
+	lg: {
+		p: 1
+	}
+};
+
+export default {
+	name: "Modals",
+	components: {
+		Modal
+	},
+	setup() {
+		const data = reactive({
+			items: []
+		});
+
+		provide('modals', data.items);
+		provide('data', data);
+
+		watch(data.items, (items) => {
+			if (items.length === 0) {
+				setTimeout(() => {
+					Ui.unlockScroll();
+				}, 0);
+			}
+		});
+
+		return {
+			data
+		}
+	},
+	provide() {
+		return {
+			scales: scales,
+			parent: this,
+			modals: this.data.items
+		}
+	},
+	mounted() {
+		this.init();
+		window.$Modal = this;
+	},
+	methods: {
+		init: _.once(function () {
+			var _this = this;
+
+			$(document).on('click', 'a[data-component-modal]', function (e) {
+				e.preventDefault();
+				var $this = $(this);
+				var path = $this.attr('data-component-modal');
+				var url = Url(`/${path}`, true);
+
+				var componentName = url.pathname.replace(/^\/+/, '');
+
+				//console.log(componentName, url);
+
+				let args = {
+					params: url.query, // for backwards compatibility
+					...url.query
+				};
+
+				_this.loadComponent(componentName, args);
+				return false;
+			});
+
+			$(document).on('click', 'a[data-modal-href]', function (e) {
+				e.preventDefault();
+				var $this = $(this);
+				var url = $this.attr('href');
+				_this.loadUrl(url);
+				return false;
+			});
+		}),
+		closeAll: function () {
+			this.data.items = [];
+		},
+		close: function () {
+			this.data.items.pop();
+		},
+		removeItem(index) {
+			this.data.items.splice(index, 1);
+		},
+		loadComponent: function (name, data, props) {
+			let modals = this.data.items;
+
+			console.log("Let's load a component", name, data, props);
+
+			modals.push({
+				props: {
+					component: {
+						name: name,
+						props: data,
+					},
+					...props
+				}
+			});
+		},
+		loadUrl: function (url) {
+			var modals = this.data.items;
+			modals.push({
+				url: url,
+			});
+		}
+	},
+	computed: {
+		isOpen() {
+			return this.nItems > 0;
+		},
+		nItems() {
+			return this.data.items.length;
+		}
+	}
+}
+</script>
+
+<style type="text/css">
+.io-modal {
+	position: fixed;
+	margin: 0 auto;
+	width: 400px;
+	border: 1px solid #ddd;
+	z-index: 9000;
+	background: #fcfcfc;
+	box-shadow: 0 3px 9px rgba(0, 0, 0, 0.5);
+}
+
+.io-modal .modal-hidden {
+	display: none;
+}
+
+.io-modal-mask {
+	z-index: 8999;
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background: #000;
+	opacity: 0.5;
+	filter: alpha(opacity=50);
+}
+
+.io-modal .io-modal-content {
+	display: block;
+	padding: 10px;
+	overflow-y: auto;
+}
+
+.io-modal .block hr {
+	margin: 20px -10px;
+}
+
+.io-modal-toolbar {
+	padding: 5px;
+	background: #efefef;
+}
+
+.io-modal .io-modal-content .content-header {
+	border-top: none;
+	margin-bottom: 20px;
+	margin-left: -10px;
+	margin-top: -10px;
+}
+
+.io-modal .block {
+	border: 1px solid #eee;
+}
+
+.io-modal ul.breadcrumb {
+	display: none;
+}
+
+.io-modal .selectize-dropdown {
+	z-index: 9000;
+}
+
+.modal,
+.modal-dialog,
+.bootstrap-dialog {
+	z-index: 9100 !important;
+}
+
+.modal-backdrop {
+	z-index: 1020 !important;
+}
+</style>
---
+++ src/modal/tmp/ModalOld.vue
@@ -0,0 +1,194 @@
+<template>
+	<div class="io-modal" :id="id">
+		<div class="io-modal-toolbar clearfix">
+			<a class="btn btn-primary btn-sm" href="#" @click="close" v-if="modals.length > 1">
+				<i class="fa fa-chevron-left"></i>
+			</a>
+			<a class="btn btn-default btn-sm float-right pull-right" href="#" @click="close" ref="closeButton">
+				<i class="fa fa-times"></i>
+			</a>
+		</div>
+		<div class="io-modal-content" ref="modalContent">
+			<div v-if="isUrl">
+				<div ref="urlContent"></div>
+			</div>
+			<div v-else>
+				<component
+				  v-if="component.name"
+				  :is="getComponentDef(component.name)"
+				  v-bind="component.props"
+				  :params="component.props"
+				></component>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import $ from 'jquery';
+import _ from 'lodash';
+import Ui from "vio/Ui";
+import {defineAsyncComponent, reactive, toRefs} from 'vue';
+
+var $body = $('body');
+var $main = $('.io-viewport').first();
+
+var loaded = {};
+
+export default {
+	name: "Modal",
+	props: {
+		data: Object,
+		size: [String],
+		url: {
+			type: String,
+			default: ''
+		},
+		component: {
+			type: Object,
+			default: null
+		}
+	},
+	inject: ['scales', 'parent', 'modals'],
+	setup(props) {
+		let propRefs = toRefs(props);
+
+		console.log('Hey, a new modal arrived', props.component);
+
+		let component = reactive({
+			name: null,
+			props: {},
+			...props.component
+		});
+
+		console.log('component', component);
+
+		return {
+			id: null,
+			token: '',
+			size: props.size || 'lg',
+			url: propRefs.url,
+			data: propRefs.data,
+			component
+		};
+	},
+	mounted() {
+		this.init();
+	},
+	unmounted() {
+		this.dispose();
+	},
+	computed: {
+		isUrl() {
+			return !!this.data.url;
+		}
+	},
+	methods: {
+		init: function () {
+			this.id = _.uniqueId('modal-');
+			this.token = Date.now();
+
+			Ui.lockScroll();
+
+			window.addEventListener('resize', this.resize);
+
+			if (this.data.url) {
+				this.loadUrl(this.data.url);
+			} else {
+
+			}
+
+			this.resize();
+		},
+		dispose: function () {
+			window.removeEventListener('resize', this.resize);
+		},
+		close: function () {
+			this.$emit('closed');
+			this.dispose();
+			return false;
+		},
+		getComponentDef(name) {
+			return defineAsyncComponent(() => import(`components/app/${name}.vue`));
+			//return require(`components/app/${name}.vue`);
+		},
+		loadComponent: function (name, data) {
+			this.componentLoaded = this.getComponentDef(name);
+
+			this.componentParams = {
+				params: data,
+				...data,
+			};
+
+			console.log('loadComponent', this.componentLoaded);
+
+		},
+		loadUrl(url) {
+			let self = this;
+			let target = this.$refs.urlContent;
+			$(target).load(url, function (content) {
+				self.resize();
+			});
+		},
+		resize: _.throttle(function () {
+			var $modalContainer = $(this.$el);
+			var $modalBody = $(this.$refs.modalContent);
+
+			var W = $main.width();
+			var H = window.innerHeight
+			  || document.documentElement.clientHeight
+			  || document.body.clientHeight;
+
+			var windowW = window.innerWidth
+			  || document.documentElement.clientWidth
+			  || document.body.clientWidth;
+
+			var gutter = 50;
+			var offset = $main.offset();
+			var size = this.size;
+
+			if (W < 900) {
+				// Force full-size when window is sufficiently small
+				size = 'lg';
+				gutter = 20;
+			}
+
+			if (W < 600) {
+				offset.left = 0;
+				W = $body.width();
+			}
+
+			if (W < 450) {
+				gutter = 10;
+			}
+
+			var scale = this.scales[size];
+
+			var modalW = Math.floor(scale.p * W) - gutter;
+			let modalH = Math.floor(scale.p * H) - gutter;
+
+			if (modalH < 750) {
+				modalH = Math.floor(H - gutter);
+			}
+
+			$modalContainer.css({
+				width: modalW,
+				height: modalH,
+				top: 0 + (gutter / 2),
+				left: offset.left + Math.floor((W - modalW) / 2),
+				marginBottom: '50px'
+			});
+
+			var containerHeight = $modalContainer.height() - 50;
+			$modalBody.css({
+				overflowX: 'hidden'
+			});
+			if (!$modalBody.hasClass('locked')) {
+				$modalBody.outerHeight(containerHeight);
+			}
+
+			return false;
+		}, 100)
+	}
+}
+</script>

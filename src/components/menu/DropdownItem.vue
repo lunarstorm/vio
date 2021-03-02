013e633133e50863081d784d6d230400a458d211
Jasper Tey <jasper.tey@gmail.com>
2021-03-02 09:08:56 -0500

    Adding generic .dropdown-item component.

---
+++ src/components/menu/DropdownItem.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="dropdown-item">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: "DropdownItem",
+	props: {
+	},
+	setup(props) {
+
+		return {}
+	},
+	computed: {},
+	mounted() {
+	}
+}
+</script>
+
+<style scoped>
+
+</style>

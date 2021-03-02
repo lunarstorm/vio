786df84292a5fdd91857d79fe2dce34f820e1ef5
Jasper Tey <jasper.tey@gmail.com>
2021-03-02 09:06:52 -0500

    Adding modal detection, and working with the new modal's availability of a modal-footer that is automatically handled by Bootstrap.

--- src/components/util/AffixBottom.vue
+++ src/components/util/AffixBottom.vue
@@ -1,25 +1,101 @@
 <template>
-	<div ref="container" class="affix-container">
-		<slot></slot>
+	<div ref="container">
+		<template v-if="teleportTo">
+			<teleport
+			  :to="teleportTo"
+			  :disabled="!teleportTo"
+			  ref="teleported"
+			>
+				<div class="modal-footer">
+					<slot></slot>
+				</div>
+			</teleport>
+		</template>
+		<div v-else>
+			<div class="affix-container">
+				<slot></slot>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
+import $ from 'jquery';
 import Ui from 'vio/Ui';
+import {ref} from 'vue';
 
 export default {
 	name: "AffixBottom",
 	setup(props) {
-		return {}
+		return {
+			teleportTo: ref(false),
+			listener: null
+		}
 	},
 	mounted() {
-		Ui.affixBottom(this.$refs.container, '#page-content');
+		let affixTo = '#page-content';
+
+		if (this.inModal()) {
+			affixTo = this.findModalContainer();
+			this.teleportTo = this.findModalFooter();
+
+			this.$nextTick(() => {
+				this.handleFormElements();
+			});
+
+		} else {
+			this.listener = Ui.affixBottom(this.$refs.container, affixTo);
+		}
 	},
+	unmounted() {
+		if (this.listener) {
+			this.listener.dispose();
+		}
+	},
+	computed: {},
+	methods: {
+		inModal() {
+			return window.$Modal && window.$Modal.isOpen;
+		},
+		findModalFooter() {
+			let $container = $(this.$refs.container);
+
+			// Find the closest modal
+			let $modal = $container.closest('.vio-modal');
+
+			return $modal.find('.vio-modal-footer')[0];
+		},
+		findModalContainer() {
+			let $container = $(this.$refs.container);
+			let $modal = $container.closest('.vio-modal');
+			return $modal.find('.vio-modal-parent')[0];
+		},
+		handleFormElements() {
+			let $container = $(this.$refs.container);
+			let $modal = $container.closest('.vio-modal');
+			let $form = $container.closest('form');
+
+			if ($form.length > 0) {
+				let formId = $form.attr('id');
+
+				if (!formId) {
+					formId = _.uniqueId('form');
+					$form.attr('id', formId);
+				}
+
+				if($modal){
+					let $submit = $modal.find('*[type=submit]');
+					$submit.attr('form', formId);
+				}
+			}
+		}
+	}
 }
 </script>
 
 <style scoped>
 .affix-container {
 	background: #fff;
+	position: relative;
 }
 </style>

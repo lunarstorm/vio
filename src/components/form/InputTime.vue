aa598b5b5d5e8b0340015a202f742408c59c928d
Jasper Tey <jasper.tey@gmail.com>
2021-03-30 22:43:40 -0400

    Handy time input components. (ft. Flatpickr)

---
+++ src/components/form/InputTime.vue
@@ -0,0 +1,67 @@
+<template>
+	<input
+	  :value="modelValue"
+	  @change="$emit('update:modelValue', $event.target.value)"
+	  ref="field"
+	  type="text"
+	  class="form-control"
+	/>
+</template>
+
+<script>
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import {toRefs} from 'vue';
+
+export default {
+	name: "InputTimeRange",
+	components: {},
+	props: {
+		modelValue: [String, Object],
+		options: {
+			type: Object,
+			default: {}
+		}
+	},
+	emits: [
+		'update:modelValue',
+	],
+	setup(props) {
+		let propRefs = toRefs(props);
+		let FP = null;
+
+		return {
+			options: propRefs.options,
+			FP
+		};
+	},
+	computed: {},
+	methods: {},
+	created() {
+	},
+	mounted() {
+		let $field = this.$refs.field;
+
+		this.FP = flatpickr($field, {
+			allowInput: true,
+			enableTime: true,
+			noCalendar: true,
+			dateFormat: "H:i",
+			//defaultDate: "13:45",
+			...this.options
+		});
+	},
+	updated() {
+		this.FP.setDate(this.modelValue, true);
+	},
+	unmounted() {
+		if (this.FP) {
+			this.FP.destroy();
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/form/InputTimeRange.vue
@@ -0,0 +1,70 @@
+<template>
+	<input-group>
+		<input-time
+		  v-model="from"
+		  @change="$emit('update:from', $event.target.value)"
+		></input-time>
+		<div class="input-group-prepend input-group-append">
+			<div class="input-group-text">
+				<slot>to</slot>
+			</div>
+		</div>
+		<input-time
+		  v-model="to"
+		  @change="$emit('update:to', $event.target.value)"
+		></input-time>
+	</input-group>
+</template>
+
+<script>
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import {toRefs} from 'vue';
+import InputGroup from "./InputGroup";
+import InputTime from "./InputTime";
+
+const DATE_FORMAT = 'yyyy-MM-dd';
+
+export default {
+	name: "InputTimeRange",
+	components: {
+		InputGroup,
+		InputTime
+	},
+	props: {
+		from: [String, Object],
+		to: [String, Object],
+		options: {
+			type: Object,
+			default: {}
+		}
+	},
+	emits: [
+		'update:from',
+		'update:to'
+	],
+	setup(props) {
+		let propRefs = toRefs(props);
+		let FP = null;
+
+		return {
+			options: propRefs.options,
+			FP
+		};
+	},
+	computed: {},
+	methods: {},
+	created() {
+	},
+	mounted() {
+	},
+	updated() {
+	},
+	unmounted() {
+	}
+}
+</script>
+
+<style scoped>
+
+</style>

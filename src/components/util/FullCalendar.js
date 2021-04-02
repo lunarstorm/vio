1e39db7cba4e9b4a2d05bba529a0be9c8fb5a3c3
Jasper Tey <jasper.tey@gmail.com>
2021-04-02 10:29:37 -0400

    WIP: FullCalendar helper.

---
+++ src/components/util/FullCalendar.js
@@ -0,0 +1,41 @@
+import "@fullcalendar/core/vdom";
+import {Calendar} from "@fullcalendar/core";
+import {
+	defineComponent,
+	h,
+	onMounted,
+	onUnmounted,
+	ref,
+	watchEffect,
+} from "vue";
+
+export default defineComponent({
+	props: {
+		options: Object,
+	},
+
+	setup(props) {
+		const el = ref();
+
+		const calendar = ref();
+
+		onMounted(() => {
+			calendar.value = new Calendar(el.value, props.options);
+			calendar.value.render();
+		});
+
+		watchEffect(() => {
+			if (calendar.value) {
+				calendar.value.pauseRendering();
+				calendar.value.resetOptions(props.options);
+				calendar.value.resumeRendering();
+			}
+		});
+
+		onUnmounted(() => {
+			calendar.value.destroy();
+		});
+
+		return () => h("div", {ref: el});
+	},
+});

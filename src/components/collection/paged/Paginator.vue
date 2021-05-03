6cea324d1be2712cb64b46f608c69a0d94d68a42
Jasper Tey <jasper.tey@gmail.com>
2021-05-03 15:50:59 -0400

    Pagination helper components, with initial support for the native pagination data spec in Laravel.

---
+++ src/components/collection/paged/Paginator.vue
@@ -0,0 +1,57 @@
+<template>
+	<div class="d-flex align-items-center">
+		<ul class="pagination mb-0">
+			<li
+			  class="paginate_button page-item previous"
+			  :class="{'disabled':!page.prev_page_url}"
+			>
+				<inertia-link
+				  v-if="page.prev_page_url"
+				  :href="page.prev_page_url"
+				  class="page-link"
+				>
+					<i class="fa fa-lg fa-angle-left mx-1"></i>
+					Prev
+				</inertia-link>
+				<div v-else class="page-link">
+					<i class="fa fa-lg fa-angle-left mx-1"></i>
+					Prev
+				</div>
+			</li>
+			<li
+			  class="paginate_button page-item next"
+			  :class="{'disabled':!page.next_page_url}"
+			>
+
+				<inertia-link
+				  v-if="page.next_page_url"
+				  :href="page.next_page_url"
+				  class="page-link"
+				>
+					Next
+					<i class="fa fa-lg fa-angle-right mx-1"></i>
+				</inertia-link>
+				<div v-else class="page-link">
+					Next
+					<i class="fa fa-lg fa-angle-right mx-1"></i>
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: "Paginator",
+	components: {
+	},
+	props: {
+		page: Object
+	}
+}
+</script>
+
+<style scoped>
+
+</style>

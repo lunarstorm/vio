fa73499d9aa9a219ea7d57152a8b050e99bb6884
Jasper Tey <jasper.tey@gmail.com>
2021-05-03 20:14:58 -0400

    Beginnings of a modal Dialog helper.

---
+++ src/components/modal/Dialog.vue
@@ -0,0 +1,91 @@
+<template>
+	<div
+	  ref="modal"
+	  class="modal"
+	>
+		<div
+		  class="modal-dialog"
+		  :class="classes"
+		>
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title">{{ title || '' }}</h5>
+				</div>
+				<div class="modal-body">
+					<div v-html="message"></div>
+				</div>
+				<div class="modal-footer">
+					<button
+					  @click.prevent="doYes"
+					  type="button" class="btn btn-primary"
+					>
+						{{ yesLabel }}
+					</button>
+					<button
+					  @click.prevent="close"
+					  type="button" class="btn btn-light"
+					>
+						{{ noLabel }}
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Dialog",
+	props: {
+		title: String,
+		message: String,
+		level: {
+			type: String,
+			default: 'info'
+		},
+		yesLabel: {
+			type: String,
+			default: 'Yes'
+		},
+		noLabel: {
+			type: String,
+			default: 'Cancel'
+		},
+		onYes: {
+			type: Function,
+			default: () => {
+				return false;
+			}
+		}
+	},
+	mounted() {
+		let $modal = $(this.$refs.modal);
+		$modal.modal();
+	},
+	computed: {
+		classes(){
+			let items = [];
+
+			//items.push()
+
+			return items;
+		}
+	},
+	methods: {
+		doYes(){
+			if(this.onYes){
+				this.onYes();
+			}
+			this.close();
+		},
+		close(){
+			let $modal = $(this.$refs.modal);
+			$modal.modal('hide');
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/helpers/Dialog.js
@@ -0,0 +1,23 @@
+import {createApp, defineAsyncComponent} from 'vue';
+
+const VioDialog = defineAsyncComponent(() => import('vio/components/modal/Dialog'));
+
+class Dialog {
+	constructor() {
+		this.component = VioDialog;
+	}
+
+	confirm(o){
+		o = {
+			title: 'Confirm',
+			message: 'Are you sure?',
+			...o
+		};
+
+		const div = document.createElement('div');
+		$('#app').append(div)
+		createApp(this.component, o).mount(div)
+	}
+}
+
+export default new Dialog();

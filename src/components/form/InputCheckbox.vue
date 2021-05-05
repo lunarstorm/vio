24c821524f01f85b07059c8186c87d48b45f2ab7
Jasper Tey <jasper.tey@gmail.com>
2021-05-04 21:46:47 -0400

    Checkbox input.

---
+++ src/components/form/InputCheckbox.vue
@@ -0,0 +1,66 @@
+<template>
+	<label v-if="inline" class="checkbox-inline">
+		<input
+		  v-model="modelValue"
+		  :value="value"
+		  @change="change($event)"
+		  :name="name"
+		  type="checkbox"
+		  class="mr-1"
+		>
+		<slot></slot>
+	</label>
+	<div v-else class="checkbox">
+		<label>
+			<input
+			  v-model="modelValue"
+			  :value="value"
+			  @change="change($event)"
+			  :name="name"
+			  type="checkbox"
+			  class="mr-1"
+			>
+			<slot></slot>
+		</label>
+	</div>
+</template>
+
+<script>
+import {toRefs} from 'vue';
+
+export default {
+	name: "InputCheckbox",
+	inheritAttrs: true,
+	components: {},
+	props: {
+		name: String,
+		modelValue: [String, Number, Array],
+		value: [String, Number],
+		inline: Boolean
+	},
+	emits: ['update:modelValue'],
+	setup(props) {
+		let propRefs = toRefs(props);
+
+		return {
+			value: propRefs.value,
+			inline: propRefs.inline
+		};
+	},
+	computed: {},
+	methods: {
+		change($event) {
+			console.log('Checkbox change', $event.target.value, this.modelValue);
+		}
+	},
+	mounted() {
+	},
+	unmounted() {
+
+	}
+}
+</script>
+
+<style scoped>
+
+</style>

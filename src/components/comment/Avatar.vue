3bc146fb3aea21fabfe4da4abf3982c9322f27db
Jasper Tey <jasper.tey@gmail.com>
2021-05-07 10:37:20 -0400

    Conversation components, based on the markup and semantics of Looper theme's conversation components.

---
+++ src/components/comment/Avatar.vue
@@ -0,0 +1,37 @@
+<template>
+	<div
+	  class="tile tile-circle"
+	  :style="styles"
+	>
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "ConversationAvatar",
+	inheritAttrs: true,
+	props: {
+		style: [Object, Array]
+	},
+	computed: {
+		styles() {
+			let styles = {};
+
+			if (this.style['background-color']) {
+				styles['background-color'] = this.style['background-color'];
+			}
+
+			if (this.style['color']) {
+				styles['color'] = this.style['color'];
+			}
+
+			return styles;
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/comment/ConversationThread.vue
@@ -0,0 +1,314 @@
+<template>
+	<div role="log" class="conversations">
+		<div v-if="params.ord == 'desc' && !params.commentId">
+			<div v-if="params.showForm && formParams">
+				<conversation-input
+				  :params="formParams"
+				  @comment-submitted="insertComment"
+				></conversation-input>
+			</div>
+			<div v-else>
+				<a
+				  @click.prevent="toggleForm"
+				  href="#"
+				  class="btn btn-sm btn-link">
+					Reply
+				</a>
+			</div>
+		</div>
+
+		<div
+		  v-if="hasPrev"
+		  class="small p-2"
+		>
+			<span class="text-muted">&mdash;</span>
+			<a @click.prevent="prev"
+			   href="#"
+			   class="text-muted"
+			>
+				Show Previous {{ noun }}
+				<span v-if="nCommentsUnshown > 0">({{ nCommentsUnshown }})</span>
+			</a>
+		</div>
+
+		<div v-if="params.totalKnown > 0 && busyLoading" class="p-2 text-center small">
+			<spinner class="text-muted"></spinner>
+		</div>
+
+		<!-- .conversation-list -->
+		<ul class="conversation-list">
+			<!-- .conversation-inbound -->
+			<template
+			  v-for="comment in comments"
+			  :key="comment.id"
+			>
+				<li class="conversation-inbound">
+					<conversation-item
+					  :key="comment.id"
+					  :comment-data="comment"
+					  :params="{id: comment.id}"
+					  :can-reply="params.canReply"
+					  :show-replies="params.showReplies && comment.numReplies > 0"
+					  @destroyed="removeComment"
+					></conversation-item>
+				</li><!-- /.conversation-inbound -->
+			</template>
+		</ul><!-- /.conversation-list -->
+
+		<div v-if="hasNext"
+			 class="text-center mb-3"
+		>
+			<a @click.prevent="next"
+			   href="#"
+			   class="btn btn-xs btn-secondary"
+			>
+				Show more...
+			</a>
+		</div>
+
+		<div v-if="params.ord == 'asc' && !params.commentId">
+			<div v-if="params.showForm">
+				<conversation-input
+				  :params="formParams"
+				  @comment-submitted="insertComment"
+				  class="my-2"
+				></conversation-input>
+			</div>
+			<div v-else>
+				<a
+				  @click.prevent="toggleForm"
+				  href="#"
+				  class="btn btn-sm btn-link"
+				>
+					Reply
+				</a>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import {
+	defineAsyncComponent,
+	reactive,
+	ref,
+	toRefs,
+	provide
+} from 'vue';
+import _ from 'lodash';
+import io from 'io/app';
+import api from 'vio/Api';
+import $ from 'jquery';
+import Spinner from 'vio/ui/Spinner.vue'
+
+const ConversationInput = defineAsyncComponent(() => import("./ConversationInput.vue"));
+const ConversationItem = defineAsyncComponent(() => import("./ConversationItem.vue"));
+
+export default {
+	name: "ConversationThread",
+	components: {
+		ConversationInput,
+		ConversationItem,
+		Spinner
+	},
+	props: {
+		params: Object,
+		hasFocus: Boolean
+	},
+	setup(props) {
+		let propRefs = toRefs(props);
+
+		let defaults = {
+			obj: null,
+			oid: null,
+			reply_id: null,
+			commentId: null,
+			parent: null,
+			showForm: true,
+			showReplies: true,
+			showThread: true,
+			toggle: false,
+			page: 1,
+			limit: 3,
+			limitNext: 10,
+			limitPrev: 10,
+			ord: 'asc',
+			hasFocus: false,
+			afterSubmit: false,
+			initials: io.data.user ? io.data.user.initials : 'NA',
+			replyingTo: false,
+			canReply: true,
+			totalKnown: 0
+		};
+
+		const params = reactive({
+			...defaults,
+			...props.params
+		});
+
+		//console.log('CommentThread props', propRefs.params);
+
+		const comments = reactive([]);
+		const hasNext = ref(false);
+		const hasPrev = ref(false);
+		const total = ref(0);
+		const page = ref(params.page);
+
+		const formParams = {
+			obj: params.obj,
+			oid: params.oid,
+			reply_id: params.reply_id,
+			hasFocus: params.hasFocus,
+			afterSubmit: function (response) {
+				params.replyingTo = false;
+			},
+			afterCancel: function () {
+				params.replyingTo = false;
+			}
+		};
+
+		const busyLoading = ref(false);
+		provide('busyLoading', busyLoading);
+
+		//console.log('setup busyLoading', busyLoading);
+
+		return {
+			formParams,
+			comments,
+			params,
+			hasNext,
+			hasPrev,
+			total,
+			page,
+			busyLoading
+		}
+	},
+	created() {
+		this.fetch({
+			type: this.params.ord == 'asc' ? 'prepend' : 'append',
+			id: this.params.commentId,
+			parent: this.params.parent
+		});
+	},
+	watch: {
+		busyLoading: function (val) {
+			console.log('Watch busyLoading', val);
+		}
+	},
+	methods: {
+		next() {
+			if (this.hasNext) {
+				this.page++;
+				this.fetch({
+					type: 'append'
+				});
+			}
+
+			return false;
+		},
+		prev() {
+			if (this.hasPrev) {
+				this.page++;
+
+				let id = null;
+
+				if (this.comments.length > 0) {
+					id = this.comments[0].id;
+				}
+
+				this.fetch({
+					type: 'prepend',
+					limit: this.limitPrev,
+					ref: id
+				});
+			}
+
+			return false;
+		},
+		fetch(o) {
+			var self = this;
+			o = $.extend({
+				id: null,
+				parent: null,
+				type: 'append',
+				limit: self.params.limit,
+				ref: null
+			}, o);
+
+			var data = {
+				obj: self.params.obj,
+				oid: self.params.oid,
+				p: self.page,
+				l: o.limit,
+				ref: o.ref,
+				ord: self.params.ord,
+				parent: self.params.parent
+			};
+
+			if (data.ref) {
+				data.p = null;
+			}
+
+			if (o.id) {
+				data['id'] = o.id;
+			}
+
+			//self.busyLoading = true;
+
+			return api.get('comments', {
+				data: data,
+				busyFlag: self.busyLoading
+			}).done(function (response) {
+				response.data.items.map(function (item) {
+					if (o.type == 'append') {
+						self.comments.push(item);
+					} else {
+						self.comments.unshift(item);
+					}
+				});
+
+				/*$.each(response.data.items, function (i, item) {
+
+				});*/
+
+				self.total = response.data.total;
+				self.hasNext = response.data.hasNext;
+				self.hasPrev = response.data.hasPrev;
+			}).always(function () {
+				//self.busyLoading = false;
+			});
+		},
+		toggleForm() {
+			this.params.showForm = !this.params.showForm;
+			return false;
+		},
+		insertComment(commentData) {
+			if (this.params.ord == 'desc') {
+				this.comments.unshift(commentData);
+			} else {
+				this.comments.push(commentData);
+			}
+		},
+		removeComment(id) {
+			let removed = _.remove(this.comments, (item) => item.id == id);
+			this.total -= removed.length;
+		}
+	},
+	computed: {
+		noun() {
+			if (this.params.parent) {
+				return 'Replies';
+			}
+
+			return 'Comments';
+		},
+		nCommentsUnshown() {
+			return this.total - this.comments.length;
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>

facb8577cf5f685a510a71687dd62b8a019514c3
Jasper Tey <jasper.tey@gmail.com>
2021-05-08 23:10:08 -0400

    Axios wrapper that provides "busy" tracking of requests; primary use case being to use with spinners.

---
+++ src/helpers/Http.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import {reactive} from "vue";
+
+class Http {
+
+	constructor() {
+		this.busy = reactive({});
+	}
+
+	create(name){
+		const instance = axios.create();
+		this.busy[name] = false;
+
+		instance.interceptors.request.use(config => {
+			this.busy[name] = true;
+			return config;
+		}, error => {
+			this.busy[name] = false;
+		});
+
+		instance.interceptors.response.use(response => {
+			this.busy[name] = false;
+			return response;
+		}, error => {
+			this.busy[name] = false;
+		});
+
+		return instance;
+	}
+
+}
+
+export default new Http();
--- src/plugin/VioPlugin.js
+++ src/plugin/VioPlugin.js
@@ -1,11 +1,13 @@
 import Dialog from "vio/helpers/Dialog";
 import Modal from "vio/helpers/Modal";
 import Messages from "vio/helpers/Messages";
+import Http from "vio/helpers/Http";
 
 export default {
 	install: (app, options) => {
 		Object.defineProperty(app.config.globalProperties, '$Dialog', {get: () => Dialog});
 		Object.defineProperty(app.config.globalProperties, '$Modal', {get: () => Modal});
 		Object.defineProperty(app.config.globalProperties, '$Messages', {get: () => Messages});
+		Object.defineProperty(app.config.globalProperties, '$http', {get: () => Http});
 	}
 }

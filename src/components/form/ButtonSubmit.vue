801f346cbf17cbe634718c963130b8f7eaf09058
Jasper Tey <jasper.tey@gmail.com>
2021-05-08 23:06:33 -0400

    Submit button wrapper, with embedded spinner option.

---
+++ src/components/form/ButtonSubmit.vue
@@ -0,0 +1,24 @@
+<template>
+	<button
+	  type="submit"
+	  class="btn btn-primary"
+	  :disabled="busy"
+	>
+		<span v-if="busy" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+		<slot>Submit</slot>
+	</button>
+</template>
+
+<script>
+export default {
+	name: "ButtonSubmit",
+	inheritAttrs: true,
+	props: {
+		busy: Boolean
+	}
+}
+</script>
+
+<style scoped>
+
+</style>

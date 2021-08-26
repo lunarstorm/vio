771a19d59e213d273e2445ca752c6bb060907394
Jasper Tey <jasper.tey@gmail.com>
2021-08-26 16:26:08 -0400

    Experimental: wrap with outer div and support <form-error> like <input-text>

--- src/components/form/OptionsMulti.vue
+++ src/components/form/OptionsMulti.vue
@@ -1,89 +1,93 @@
 <template>
-	<multi-select
-	  v-model="modelValue"
-	  @change="$emit('update:modelValue', $event)"
-	  :options="optionsNormalized"
-	  :searchable="true"
-	  :placeholder="_placeholder"
+  <div>
+    <multi-select
+      v-model="modelValue"
+      @change="$emit('update:modelValue', $event)"
+      :options="optionsNormalized"
+      :searchable="true"
+      :placeholder="_placeholder"
       :close-on-select="true"
-	  ref="multiselect"
-	>
-		<slot></slot>
-	</multi-select>
+      ref="multiselect"
+      v-bind="$attrs"
+    >
+      <slot></slot>
+    </multi-select>
+    <form-error v-model="error"></form-error>
+  </div>
 </template>
 
 <script>
 import "@vueform/multiselect/themes/default.scss";
-import MultiSelect from '@vueform/multiselect';
+import MultiSelect from "@vueform/multiselect";
 import FormOptions from "vio/helpers/FormOptions";
-import _ from 'lodash';
+import FormError from "vio/components/form/FormError";
+import _ from "lodash";
 
 export default {
-	name: "OptionsMulti",
-	inheritAttrs: true,
-	components: {
-		MultiSelect,
-	},
-	emits: ['update:modelValue'],
-	props: {
-		modelValue: [String, Number, Array],
-		placeholder: [String, Number],
-		options: {
-			type: [Array, Object, Function],
-			defaultValue: []
-		},
-		async: Boolean,
-		clearOption: {
-			type: Boolean,
-			defaultValue: true
-		}
-	},
-	setup(props) {
-		return {};
-	},
-	computed: {
-		optionsNormalized() {
-			if (this.async) {
-				return this.options;
-			}
+  name: "OptionsMulti",
+  inheritAttrs: false,
+  components: {
+    MultiSelect,
+    FormError,
+  },
+  emits: ["update:modelValue"],
+  props: {
+    modelValue: [String, Number, Array],
+    placeholder: [String, Number],
+    options: {
+      type: [Array, Object, Function],
+      defaultValue: [],
+    },
+    async: Boolean,
+    clearOption: {
+      type: Boolean,
+      defaultValue: true,
+    },
+    error: [Array, String],
+  },
+  setup(props) {
+    return {};
+  },
+  computed: {
+    optionsNormalized() {
+      if (this.async) {
+        return this.options;
+      }
 
-			return FormOptions.normalize(this.options);
-		},
-		selectedValueExists() {
-			if (!this.modelValue) {
-				return true;
-			}
+      return FormOptions.normalize(this.options);
+    },
+    selectedValueExists() {
+      if (!this.modelValue) {
+        return true;
+      }
 
-			let opts = this.optionsNormalized;
-			let index = _.findIndex(opts, item => {
-				if(!item){
-					return false;
-				}
+      let opts = this.optionsNormalized;
+      let index = _.findIndex(opts, (item) => {
+        if (!item) {
+          return false;
+        }
 
-				return item.value == this.modelValue;
-			})
-			return index > -1;
-		},
-		_placeholder() {
-			if (!this.selectedValueExists) {
-				return `${this.modelValue}`;
-			}
+        return item.value == this.modelValue;
+      });
+      return index > -1;
+    },
+    _placeholder() {
+      if (!this.selectedValueExists) {
+        return `${this.modelValue}`;
+      }
 
-			return this.placeholder;
-		}
-	},
-	methods: {
-		clear() {
-			this.$refs.multiselect.clear()
-		}
-	},
-	mounted() {
-	},
-	unmounted() {
-	}
-}
+      return this.placeholder;
+    },
+  },
+  methods: {
+    clear() {
+      this.$refs.multiselect.clear();
+    },
+  },
+  mounted() {},
+  unmounted() {},
+};
 </script>
 
 <style scoped>
-
 </style>

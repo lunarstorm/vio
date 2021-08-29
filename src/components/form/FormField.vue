a8626bc6dbcf7703cae69d0b8536122795c9ba5c
Jasper Tey <jasper.tey@gmail.com>
2021-08-28 22:57:46 -0400

    FormField wrapper component; - auto-attaches <form-error> below the <slot> - decouples error-handling concerns from other individual form element components

---
+++ src/components/form/FormField.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <slot></slot>
+    <form-error v-model="error"></form-error>
+  </div>
+</template>
+
+<script>
+import FormError from "vio/components/form/FormError";
+
+export default {
+  name: "FormField",
+  inheritAttrs: true,
+  components: {
+    FormError,
+  },
+  props: {
+    error: [Array, String, Number],
+  },
+  setup(props) {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  mounted() {},
+  unmounted() {},
+};
+</script>
+
+<style scoped>
+</style>

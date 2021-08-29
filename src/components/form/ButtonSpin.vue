145893de003f5bfe1fa11fb7bac8dbdd12d8ee24
Jasper Tey <jasper.tey@gmail.com>
2021-08-28 23:15:25 -0400

    More generic button wrapper with spinner capability; - a less opinionated version of ButtonSubmit

---
+++ src/components/form/ButtonSpin.vue
@@ -0,0 +1,23 @@
+<template>
+	<button
+	  type="button"
+	  :disabled="busy"
+	>
+		<span v-if="busy" class="spinner-border spinner-border-sm mr-1" role="status" aria-hidden="true"></span>
+		<slot></slot>
+	</button>
+</template>
+
+<script>
+export default {
+	name: "ButtonSpin",
+	inheritAttrs: true,
+	props: {
+		busy: Boolean
+	}
+}
+</script>
+
+<style scoped>
+
+</style>

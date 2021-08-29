560ffed6194048a58618f02aba1d5057b9200ffd
Jasper Tey <jasper.tey@gmail.com>
2021-08-28 21:21:34 -0400

    1st draft of vio Form helper: - inspired by inertiajs form helper - auto-magically handle the 422 validation errors, among other things

---
+++ src/helpers/Form.js
@@ -0,0 +1,121 @@
+import { reactive, watch } from 'vue';
+import _ from 'lodash';
+import Http from "vio/helpers/Http";
+
+class Form {
+    constructor(data) {
+        let root = this;
+
+        this.id = _.uniqueId('form');
+        this._http = Http.make(this.id);
+
+        this.config = {
+            method: 'post',
+            url: window.location.href
+        };
+
+        this.state = reactive({
+            data: {
+                ...data
+            },
+            errors: {},
+        });
+
+        this.data = new Proxy(this.state, {
+            get(target, prop) {
+                if (typeof prop === 'undefined') {
+                    return target.data;
+                }
+
+                try {
+                    return target.data[prop];
+                }
+                catch (e) {
+
+                }
+
+                return null;
+            },
+            set(target, prop, value) {
+                target.data[prop] = value;
+                return true;
+            }
+        });
+
+        this.errors = new Proxy(this.state, {
+            get(target, prop) {
+                if (typeof prop === 'undefined') {
+                    return target.errors;
+                }
+
+                try {
+                    return target.errors[prop];
+                }
+                catch (e) {
+
+                }
+
+                return null;
+            },
+            set(target, prop, value) {
+                target.errors[prop] = value;
+                return true;
+            }
+        });
+    }
+
+    static make(config) {
+        return new Form(config);
+    }
+
+    method(value) {
+        this.config.method = typeof value === 'undefined' ? 'post' : value;
+        return this;
+    }
+
+    url(value) {
+        this.config.url = typeof value === 'undefined' ? '/' : value;
+        return this;
+    }
+
+    clearErrors() {
+        Object.assign(
+            this.state.errors,
+            _.mapValues(this.state.errors, () => null)
+        );
+    }
+
+    fillErrors(errors) {
+        this.clearErrors();
+        Object.assign(this.state.errors, errors);
+    }
+
+    isBusy() {
+        return Http.isBusy(this.id);
+    }
+
+    submit(url) {
+        url = url || this.url;
+        let method = "post";
+        let payload = this.state.data;
+
+        if (payload.id) {
+            method = "put";
+            url += `/${payload.id}`;
+        }
+
+        return this._http
+            .request({
+                url: url,
+                method: method,
+                data: payload,
+            })
+            .catch((error) => {
+                if (error.response) {
+                    this.fillErrors(error.response.data.errors);
+                }
+            });
+    }
+}
+
+export default Form;

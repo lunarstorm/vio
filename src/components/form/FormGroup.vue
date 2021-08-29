5242a5b0f4c326b685997c734de3e69de4e17690
Jasper Tey <jasper.tey@gmail.com>
2021-08-29 11:32:55 -0400

    FormGroup wrapper component; - uses FormField helper internally

---
+++ src/components/form/FormGroup.vue
@@ -0,0 +1,30 @@
+<template>
+  <form-field :error="error" class="form-group">
+    <slot></slot>
+  </form-field>
+</template>
+
+<script>
+import FormField from "vio/components/form/FormField";
+
+export default {
+  name: "FormGroup",
+  inheritAttrs: true,
+  components: {
+    FormField,
+  },
+  props: {
+    error: [Array, String, Number],
+  },
+  setup(props) {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  mounted() {},
+  unmounted() {},
+};
+</script>
+
+<style scoped>
+</style>

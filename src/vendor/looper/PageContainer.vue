a9e5ea637729d2c2c73663c6988d82dffe3c40e1
Jasper Tey <jasper.tey@gmail.com>
2021-09-03 19:53:11 -0400

    Experimental tweaks and new props to customize sidebar.

--- src/vendor/looper/PageContainer.vue
+++ src/vendor/looper/PageContainer.vue
@@ -1,107 +1,115 @@
 <template>
-	<div
-	  ref="page"
-	  class="page"
-	  :class="pageClasses()"
-	>
-		<div v-if="hasSidebar()" class="sidebar-backdrop"></div>
+  <div ref="page" class="page" :class="pageClasses()">
+    <div v-if="hasSidebar()" class="sidebar-backdrop"></div>
 
-		<div class="page-inner p-2">
-			<button
-			  v-if="hasSidebar()"
-			  class="btn btn-success btn-floated d-xl-none"
-			  type="button"
-			  data-toggle="sidebar"
-			>
-				<slot name="sidebar-toggle">
-					<i class="fa fa-bars"></i>
-				</slot>
-			</button>
+    <div class="page-inner p-2" :style="pageInnerStyle">
+      <button
+        v-if="hasSidebar()"
+        class="btn btn-success btn-floated d-xl-none"
+        type="button"
+        data-toggle="sidebar"
+      >
+        <slot name="sidebar-toggle">
+          <i class="fa fa-bars"></i>
+        </slot>
+      </button>
 
-			<header v-if="!!$slots.titlebar" class="page-title-bar">
-				<slot name="titlebar"></slot>
-			</header>
+      <header v-if="!!$slots.titlebar" class="page-title-bar">
+        <slot name="titlebar"></slot>
+      </header>
 
-			<div class="page-section">
-				<slot></slot>
-			</div>
-		</div>
+      <div class="page-section">
+        <slot></slot>
+      </div>
+    </div>
 
-		<div v-if="hasSidebar()" class="page-sidebar">
-			<header class="sidebar-header d-sm-none">
-				<nav aria-label="breadcrumb">
-					<ol class="breadcrumb">
-						<li class="breadcrumb-item active">
-							<a
-							  href="#"
-							  @click.prevent="toggleSidebar"
-							>
-								<i class="breadcrumb-icon fa fa-angle-left mr-2"></i>
-								Back
-							</a>
-						</li>
-					</ol>
-				</nav>
-			</header>
-			<div
-			  :class="sidebarClasses()"
-			>
-				<slot name="sidebar"></slot>
-			</div>
-		</div>
-	</div>
+    <div v-if="hasSidebar()" class="page-sidebar" :style="sidebarStyle()">
+      <header class="sidebar-header d-sm-none">
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item active">
+              <a href="#" @click.prevent="toggleSidebar">
+                <i class="breadcrumb-icon fa fa-angle-left mr-2"></i>
+                Back
+              </a>
+            </li>
+          </ol>
+        </nav>
+      </header>
+      <div :class="sidebarClasses()">
+        <slot name="sidebar"></slot>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-	name: "PageContainer",
-	props: {
-		sidebarFluid: {
-			type: Boolean,
-			default: false
-		},
-		sidebarFill: Boolean
-	},
-	mounted() {
+  name: "PageContainer",
+  props: {
+    sidebarFluid: {
+      type: Boolean,
+      default: false,
+    },
+    sidebarFill: Boolean,
+    sidebarWidth: [String],
+  },
+  mounted() {},
+  computed: {
+    pageInnerStyle() {
+      let style = {};
 
-	},
-	computed: {},
-	methods: {
-		toggleSidebar(){
-			Looper.toggleSidebar();
-		},
-		hasSidebar() {
-			return !!this.$slots.sidebar;
-		},
-		pageClasses() {
-			let items = [];
+      if (this.sidebarWidth) {
+        style["margin-right"] = this.sidebarWidth;
+      }
 
-			if (this.hasSidebar()) {
-				items.push('has-sidebar has-sidebar-expand-xl')
-			}
+      return style;
+    },
+  },
+  methods: {
+    toggleSidebar() {
+      Looper.toggleSidebar();
+    },
+    hasSidebar() {
+      return !!this.$slots.sidebar;
+    },
+    pageClasses() {
+      let items = [];
 
-			if (this.sidebarFluid) {
-				items.push('has-sidebar-fluid');
-			}
+      if (this.hasSidebar()) {
+        items.push("has-sidebar has-sidebar-expand-xl");
+      }
 
-			return items;
-		},
-		sidebarClasses() {
-			let items = [];
+      if (this.sidebarFluid) {
+        items.push("has-sidebar-fluid");
+      }
 
-			if (this.sidebarFill) {
-				items.push('sidebar-section-fill');
-			}
-			else{
-				items.push('sidebar-section');
-			}
+      return items;
+    },
+    sidebarClasses() {
+      let items = [];
 
-			return items;
-		}
-	}
-}
+      if (this.sidebarFill) {
+        items.push("sidebar-section-fill");
+      } else {
+        items.push("sidebar-section");
+      }
+
+      return items;
+    },
+    sidebarStyle() {
+      let style = {};
+
+      if (this.sidebarWidth) {
+        style["width"] = this.sidebarWidth;
+        style["max-width"] = this.sidebarWidth;
+      }
+
+      return style;
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 </style>

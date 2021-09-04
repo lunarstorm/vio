7dd9236daa59cbfec0e1821c7e1889331b8762a3
Jasper Tey <jasper.tey@gmail.com>
2021-09-04 01:36:08 -0400

    Remove wrapping div, which causes issues for input groups.

--- src/components/form/InputText.vue
+++ src/components/form/InputText.vue
@@ -1,48 +1,42 @@
 <template>
-	<div>
-		<label v-if="label">{{ label }}</label>
-		<input
-		  :value="modelValue"
-		  @input="$emit('update:modelValue', $event.target.value)"
-		  ref="field"
-		  type="text"
-		  class="form-control"
-		  v-bind="$attrs"
-		/>
-		<form-error v-model="error"></form-error>
-	</div>
+  <label v-if="label">{{ label }}</label>
+  <input
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    ref="field"
+    type="text"
+    class="form-control"
+    v-bind="$attrs"
+  />
+  <form-error v-model="error"></form-error>
 </template>
 
 <script>
 import FormError from "vio/components/form/FormError";
 
 export default {
-	name: "InputText",
-	inheritAttrs: false,
-	components: {
-		FormError
-	},
-	props: {
-		modelValue: [String, Number],
-		label: [String],
-		error: [Array, String]
-	},
-	emits: ['update:modelValue'],
-	setup(props) {
-		return {
-			options: props.options
-		};
-	},
-	computed: {},
-	methods: {},
-	mounted() {
-
-	},
-	unmounted() {
-	}
-}
+  name: "InputText",
+  inheritAttrs: false,
+  components: {
+    FormError,
+  },
+  props: {
+    modelValue: [String, Number],
+    label: [String],
+    error: [Array, String],
+  },
+  emits: ["update:modelValue"],
+  setup(props) {
+    return {
+      options: props.options,
+    };
+  },
+  computed: {},
+  methods: {},
+  mounted() {},
+  unmounted() {},
+};
 </script>
 
 <style scoped>
-
 </style>

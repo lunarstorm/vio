063d10b8682b80d402c7c828fa68d2b5edd076e0
Jasper Tey <jasper.tey@gmail.com>
2021-09-04 01:35:35 -0400

    Allow customization of the "alt" class.

--- src/components/form/InputGroup.vue
+++ src/components/form/InputGroup.vue
@@ -1,41 +1,51 @@
 <template>
-	<div class="input-group input-group-alt">
-		<div v-if="!!$slots.start" class="input-group-prepend">
-			<slot name="start"></slot>
-		</div>
-		<label v-if="!!$slots.before" class="input-group-prepend">
-			<span class="input-group-text"><slot name="before"></slot></span>
-		</label>
-		<slot></slot>
-		<label v-if="!!$slots.after" class="input-group-append">
-			<span class="input-group-text"><slot name="after"></slot></span>
-		</label>
-		<div v-if="!!$slots.end" class="input-group-append">
-			<slot name="end"></slot>
-		</div>
-	</div>
+  <div class="input-group" :class="classes">
+    <div v-if="!!$slots.start" class="input-group-prepend">
+      <slot name="start"></slot>
+    </div>
+    <label v-if="!!$slots.before" class="input-group-prepend">
+      <span class="input-group-text"><slot name="before"></slot></span>
+    </label>
+    <slot></slot>
+    <label v-if="!!$slots.after" class="input-group-append">
+      <span class="input-group-text"><slot name="after"></slot></span>
+    </label>
+    <div v-if="!!$slots.end" class="input-group-append">
+      <slot name="end"></slot>
+    </div>
+  </div>
 </template>
 
 <script>
-
 export default {
-	name: "InputGroup",
-	inheritAttrs: true,
-	components: {},
-	props: {},
-	setup(props) {
-		return {};
-	},
-	computed: {},
-	methods: {},
-	mounted() {
-	},
-	unmounted() {
+  name: "InputGroup",
+  inheritAttrs: true,
+  components: {},
+  props: {
+    alt: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  setup(props) {
+    return {};
+  },
+  computed: {
+    classes() {
+      let c = [];
 
-	}
-}
+      if (this.alt) {
+        c.push("input-group-alt");
+      }
+
+      return c;
+    },
+  },
+  methods: {},
+  mounted() {},
+  unmounted() {},
+};
 </script>
 
 <style scoped>
-
 </style>

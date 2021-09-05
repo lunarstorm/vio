eb9a3c218d6209c93d39141d0cc2841ffde0747d
Jasper Tey <jasper.tey@gmail.com>
2021-09-04 22:10:42 -0400

    Wrapper component to "sort array in a modal"; filing under ux with the intent to separate use-case centric components.

---
+++ src/ux/sorting/ModalArraySorter.vue
@@ -0,0 +1,69 @@
+<template>
+  <modal size="lg" :center="true" :scroll="false">
+    <template #title>
+      <slot name="title">Sort Items </slot>
+    </template>
+    <template #default>
+      <table class="table">
+        <vue-draggable
+          :list="modelValue"
+          :itemKey="itemKey"
+          :group="group"
+          tag="tbody"
+          handle=".handle"
+        >
+          <template #item="{ element: item, index }">
+            <tr>
+              <td width="15" class="handle text-center px-0 align-middle">
+                <i
+                  class="bi-grip-vertical text-muted"
+                  style="font-size: 1.5rem"
+                ></i>
+              </td>
+              <td class="align-middle">
+                <slot name="item" :item="item" :index="index"></slot>
+              </td>
+            </tr>
+          </template>
+        </vue-draggable>
+      </table>
+    </template>
+    <template #footer="{ close }">
+      <button @click.prevent="close" class="btn btn-primary">Dismiss</button>
+    </template>
+  </modal>
+</template>
+
+<script>
+import Modal from "vio/components/modal/Modal";
+import VueDraggable from "vuedraggable";
+
+export default {
+  name: "ModalArraySorter",
+  components: {
+    Modal,
+    VueDraggable,
+  },
+  props: {
+    modelValue: [Array],
+    itemKey: String,
+    group: {
+      type: String,
+      default: () => {
+        let timestamp = +new Date();
+        return "sort-group-" + timestamp;
+      },
+    },
+  },
+  emits: ["submit"],
+  setup(props) {},
+  methods: {},
+};
+</script>
+
+<style>
+.handle {
+  cursor: move;
+  cursor: -webkit-grabbing;
+}
+</style>

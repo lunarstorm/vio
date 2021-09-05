20c172768febec0b5a378a054a0943e5ad061682
Jasper Tey <jasper.tey@gmail.com>
2021-09-04 20:17:53 -0400

    Code auto-format.

--- src/components/form/OptionsSelect.vue
+++ src/components/form/OptionsSelect.vue
@@ -1,66 +1,60 @@
 <template>
-	<select
-	  v-model="modelValue"
-	  @change="$emit('update:modelValue', $event.target.value)"
-	  :name="name"
-	  class="form-control"
-	>
-		<option v-if="placeholder" :value="null" disabled>{{ caption }}</option>
-		<option
-		  v-for="opt in optionsParsed"
-		  :value="opt.value"
-		>
-			{{ opt.text }}
-		</option>
-	</select>
+  <select
+    v-model="modelValue"
+    @change="$emit('update:modelValue', $event.target.value)"
+    :name="name"
+    class="form-control"
+  >
+    <option v-if="placeholder" :value="null" disabled>{{ caption }}</option>
+    <option v-for="opt in optionsParsed" :value="opt.value" :key="opt.value">
+      {{ opt.text }}
+    </option>
+  </select>
 </template>
 
 <script>
 import FormOptions from "vio/helpers/FormOptions";
-import {toRefs} from "vue";
+import { toRefs } from "vue";
 
 export default {
-	name: "OptionsSelect",
-	inheritAttrs: true,
-	components: {},
-	props: {
-		name: {
-			type: String,
-			default: ''
-		},
-		placeholder: {
-			type: Boolean,
-			default: true
-		},
-		caption: {
-			type: String,
-			default: 'Select an option'
-		},
-		modelValue: String,
-		options: {
-			type: [Array, Object],
-			default: []
-		}
-	},
-	setup(props) {
-		return {
-			options: toRefs(props).options,
-			caption: props.caption
-		};
-	},
-	computed: {
-		optionsParsed() {
-			return FormOptions.normalize(this.options);
-		}
-	},
-	methods: {},
-	mounted() {
-	},
-	unmounted() {
-	}
-}
+  name: "OptionsSelect",
+  inheritAttrs: true,
+  components: {},
+  props: {
+    name: {
+      type: String,
+      default: "",
+    },
+    placeholder: {
+      type: Boolean,
+      default: true,
+    },
+    caption: {
+      type: String,
+      default: "Select an option",
+    },
+    modelValue: String,
+    options: {
+      type: [Array, Object],
+      default: [],
+    },
+  },
+  setup(props) {
+    return {
+      options: toRefs(props).options,
+      caption: props.caption,
+    };
+  },
+  computed: {
+    optionsParsed() {
+      return FormOptions.normalize(this.options);
+    },
+  },
+  methods: {},
+  mounted() {},
+  unmounted() {},
+};
 </script>
 
 <style scoped>
-
 </style>

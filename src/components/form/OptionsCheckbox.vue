5aea50fed0ff63f4a9e24d26e9f188bfe3d78bd6
Jasper Tey <jasper.tey@gmail.com>
2021-09-05 22:03:14 -0400

    Fixing OptionsCheckbox counterpart to work as per recent checkbox fixes.

--- src/components/form/OptionsCheckbox.vue
+++ src/components/form/OptionsCheckbox.vue
@@ -1,63 +1,68 @@
 <template>
-	<input-checkbox
-	  v-for="(opt, index) in optionsParsed"
-	  :key="index"
-	  v-model="modelValue"
-	  :value="opt.value"
-	  @change="change($event)"
-	  :inline="inline"
-	>
-		{{ opt.text }}
-	</input-checkbox>
-
-	<pre>{{ modelValue }}</pre>
+  <input-checkbox
+    v-for="(opt, index) in optionsParsed"
+    :key="index"
+    v-model="model"
+    :value="opt.value"
+    :inline="inline"
+  >
+    {{ opt.text }}
+  </input-checkbox>
 </template>
 
 <script>
 import FormOptions from "vio/helpers/FormOptions";
-import {toRefs} from 'vue';
-import InputCheckbox from 'vio/components/form/InputCheckbox';
+import { toRefs } from "vue";
+import InputCheckbox from "vio/components/form/InputCheckbox";
 
 export default {
-	name: "OptionsCheckbox",
-	inheritAttrs: true,
-	components: {
-		InputCheckbox
-	},
-	props: {
-		name: String,
-		modelValue: [String, Number, Array],
-		options: {
-			type: [Array, Object],
-			default: []
-		},
-		inline: Boolean
-	},
-	emits: ['update:modelValue'],
-	setup(props) {
-		return {
-			options: toRefs(props).options,
-			inline: props.inline
-		};
-	},
-	computed: {
-		optionsParsed(){
-			return FormOptions.normalize(this.options);
-		}
-	},
-	methods: {
-		change($event){
-			console.log('Options Checkbox change', $event.target.value, this.modelValue);
-		}
-	},
-	mounted() {
-	},
-	unmounted() {
-
-	}
-}
+  name: "OptionsCheckbox",
+  inheritAttrs: true,
+  components: {
+    InputCheckbox,
+  },
+  props: {
+    name: String,
+    modelValue: [String, Number, Array],
+    options: {
+      type: [Array, Object],
+      default: [],
+    },
+    inline: Boolean,
+  },
+  emits: ["update:modelValue"],
+  setup(props) {
+    return {
+      options: toRefs(props).options,
+      inline: props.inline,
+    };
+  },
+  computed: {
+    optionsParsed() {
+      return FormOptions.normalize(this.options);
+    },
+    model: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit("update:modelValue", value);
+      },
+    },
+  },
+  methods: {
+    change($event) {
+      console.log(
+        "Options Checkbox change",
+        $event.target.value,
+        this.modelValue
+      );
+    },
+  },
+  mounted() {},
+  unmounted() {},
+};
 </script>
 
 <style scoped>
-
 </style>

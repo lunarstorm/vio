814005cf989b0722d1ea0b58f54d2ff0b3717413
Jasper Tey <jasper.tey@gmail.com>
2021-09-05 22:29:00 -0400

    Code auto-format.

--- src/components/form/InputRadio.vue
+++ src/components/form/InputRadio.vue
@@ -1,62 +1,58 @@
 <template>
-	<label v-if="inline" class="radio-inline">
-		<input
-		  v-model="modelValue"
-		  :value="value"
-		  @input="$emit('update:modelValue', $event.target.value)"
-		  :name="name"
-		  type="radio"
-		  class="mr-1"
-		>
-		<slot></slot>
-	</label>
-	<div v-else class="radio">
-		<label>
-			<input
-			  v-model="modelValue"
-			  :value="value"
-			  @input="$emit('update:modelValue', $event.target.value)"
-			  :name="name"
-			  type="radio"
-			  class="mr-1"
-			>
-			<slot></slot>
-		</label>
-	</div>
+  <label v-if="inline" class="radio-inline">
+    <input
+      v-model="modelValue"
+      :value="value"
+      @input="$emit('update:modelValue', $event.target.value)"
+      :name="name"
+      type="radio"
+      class="mr-1"
+    />
+    <slot></slot>
+  </label>
+  <div v-else class="radio">
+    <label>
+      <input
+        v-model="modelValue"
+        :value="value"
+        @input="$emit('update:modelValue', $event.target.value)"
+        :name="name"
+        type="radio"
+        class="mr-1"
+      />
+      <slot></slot>
+    </label>
+  </div>
 </template>
 
 <script>
-import {toRefs} from 'vue';
+import { toRefs } from "vue";
 
 export default {
-	name: "InputRadio",
-	inheritAttrs: true,
-	components: {},
-	props: {
-		name: String,
-		modelValue: [String, Number, Boolean],
-		value: [String, Number, Boolean],
-		inline: Boolean
-	},
-	emits: ['update:modelValue'],
-	setup(props) {
-		let propRefs = toRefs(props);
+  name: "InputRadio",
+  inheritAttrs: true,
+  components: {},
+  props: {
+    name: String,
+    modelValue: [String, Number, Boolean],
+    value: [String, Number, Boolean],
+    inline: Boolean,
+  },
+  emits: ["update:modelValue"],
+  setup(props) {
+    let propRefs = toRefs(props);
 
-		return {
-			value: propRefs.value,
-			inline: propRefs.inline
-		};
-	},
-	computed: {},
-	methods: {},
-	mounted() {
-	},
-	unmounted() {
-
-	}
-}
+    return {
+      value: propRefs.value,
+      inline: propRefs.inline,
+    };
+  },
+  computed: {},
+  methods: {},
+  mounted() {},
+  unmounted() {},
+};
 </script>
 
 <style scoped>
-
 </style>

bd5e29a639b80af2547f5af1a121621c0f31d717
Jasper Tey <jasper.tey@gmail.com>
2021-09-06 22:28:17 -0400

    Add commonly used search filter style field.

---
+++ src/ux/SearchFilter/FilterField.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="form-group">
+    <label>{{ label }}</label>
+    <small class="ml-1">
+      <a @click.prevent="$emit('update:modelValue', null)" href="#"> clear </a>
+    </small>
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FilterField",
+  components: {},
+  props: {
+    label: String,
+    modelValue: [String, Number, Boolean],
+  },
+  emits: ["update:modelValue"],
+  setup(props) {},
+  methods: {},
+};
+</script>
+
+<style>
+</style>

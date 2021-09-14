63bd9e7674adadf607a30fd5cfa67cf26fb93a38
Jasper Tey <jasper.tey@gmail.com>
2021-09-13 23:04:30 -0400

    New TextDollar helper for commonly used money use cases.

---
+++ src/components/text/TextDollars.vue
@@ -0,0 +1,29 @@
+<template>
+  <text-numeric :value="value" format="0,0.00">
+    <template v-if="symbol">{{ symbol }}</template>
+  </text-numeric>
+</template>
+
+<script>
+import TextNumeric from "vio/components/text/TextNumeric";
+
+export default {
+  name: "TextDollars",
+  components: {
+    TextNumeric,
+  },
+  props: {
+    value: [String, Number],
+    symbol: {
+      type: [String, Boolean],
+      default: "$",
+    },
+  },
+  setup(props) {},
+  methods: {},
+  computed: {},
+};
+</script>
+
+<style scoped>
+</style>

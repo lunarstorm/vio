b841028a9801529012f4c72090c9e3455f94f0b4
Jasper Tey <jasper.tey@gmail.com>
2021-09-15 09:15:31 -0400

    Add LinkAddress component from external project.

---
+++ src/components/link/LinkAddress.vue
@@ -0,0 +1,45 @@
+<template>
+  <a
+    :href="href"
+    class="text-muted"
+    title="Open in Google Maps"
+    target="_blank"
+  >
+    <i class="fa fa-map-marker"></i> {{ addressString }}
+  </a>
+</template>
+
+<script>
+import { ref } from "vue";
+import _ from "lodash";
+
+export default {
+  name: "LinkAddress",
+  props: {
+    address: {
+      type: [String, Array],
+      default: null,
+    },
+  },
+  setup(props) {
+    return {};
+  },
+  computed: {
+    href() {
+      return `//maps.google.com/?q=${this.addressString}`;
+    },
+    addressString() {
+      let str = this.address;
+      if (_.isArray(str)) {
+        str = this.address.join(", ");
+      }
+
+      return str.trim();
+    },
+  },
+  mounted() {},
+};
+</script>
+
+<style scoped>
+</style>

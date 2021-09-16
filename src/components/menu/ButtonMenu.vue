77b0ec05dd68a1c360ed4daa82b6cd12858dd3b0
Jasper Tey <jasper.tey@gmail.com>
2021-09-15 20:55:02 -0400

    Autoformatting.

--- src/components/menu/ButtonMenu.vue
+++ src/components/menu/ButtonMenu.vue
@@ -1,62 +1,59 @@
 <template>
-	<div class="btn-group">
-		<button
-		  v-bind="$attrs"
-		  type="button"
-		  class="btn dropdown-toggle"
-		  data-toggle="dropdown"
-		>
-			<slot>Actions</slot>
-		</button>
-		<div :class="cssDropdownMenu" class="dropdown-menu" role="menu">
-			<dropdown-items :items="items"></dropdown-items>
-		</div>
-	</div>
+  <div class="btn-group">
+    <button
+      v-bind="$attrs"
+      type="button"
+      class="btn dropdown-toggle"
+      data-toggle="dropdown"
+    >
+      <slot>Actions</slot>
+    </button>
+    <div :class="cssDropdownMenu" class="dropdown-menu" role="menu">
+      <dropdown-items :items="items"></dropdown-items>
+    </div>
+  </div>
 </template>
 
 <script>
-import {toRefs} from 'vue';
-import DropdownItems from './DropdownItems.vue';
+import { toRefs } from "vue";
+import DropdownItems from "./DropdownItems.vue";
 
 export default {
-	name: "ButtonMenu",
-	inheritAttrs: false,
-	props: {
-		items: {
-			type: Array,
-			default: []
-		},
-		align: {
-			type: String,
-			default: 'left'
-		}
-	},
-	components: {
-		DropdownItems
-	},
-	setup(props) {
+  name: "ButtonMenu",
+  inheritAttrs: false,
+  props: {
+    items: {
+      type: Array,
+      default: [],
+    },
+    align: {
+      type: String,
+      default: "left",
+    },
+  },
+  components: {
+    DropdownItems,
+  },
+  setup(props) {
+    return {
+      items: toRefs(props).items,
+      align: props.align,
+    };
+  },
+  computed: {
+    cssDropdownMenu() {
+      let classes = [];
 
-		return {
-			items: toRefs(props).items,
-			align: props.align
-		}
-	},
-	computed: {
-		cssDropdownMenu() {
-			let classes = [];
+      if (this.align == "right") {
+        classes.push("dropdown-menu-right");
+      }
 
-			if (this.align == 'right') {
-				classes.push('dropdown-menu-right');
-			}
-
-			return classes;
-		}
-	},
-	mounted() {
-	}
-}
+      return classes;
+    },
+  },
+  mounted() {},
+};
 </script>
 
 <style scoped>
-
 </style>

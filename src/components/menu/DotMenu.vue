9dc2bc891ddff303b5022857d971e284faeed2bf
Jasper Tey <jasper.tey@gmail.com>
2021-09-15 20:55:45 -0400

    Light wrapper for generating the 3-dots menu more easily.

---
+++ src/components/menu/DotMenu.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="btn-group">
+    <slot name="before"></slot>
+    <button
+      type="button"
+      class="btn btn-light p-1"
+      data-toggle="dropdown"
+      data-boundary="viewport"
+      aria-haspopup="true"
+      aria-expanded="false"
+    >
+      <i class="bi bi-three-dots"></i>
+    </button>
+    <div class="dropdown-menu dropdown-menu-right">
+      <slot name="items"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "DotMenu",
+  inheritAttrs: false,
+  props: {
+  },
+  components: {
+  },
+  setup(props) {
+    return {
+    };
+  },
+  computed: {
+  },
+  mounted() {},
+};
+</script>
+
+<style scoped>
+</style>

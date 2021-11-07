90776055c6194dfb23493236a478f0c2a1fdd660
Jasper Tey <jasper.tey@gmail.com>
2021-11-07 18:47:48 -0500

    Batch selection WIP.

--- src/components/collection/ArrayTableSortable.vue
+++ src/components/collection/ArrayTableSortable.vue
@@ -3,8 +3,25 @@
     <table class="table" v-bind="$attrs">
       <thead v-if="!!$slots.head" :class="headClass">
         <tr>
+          <th v-if="selectable" class="text-center px-0 align-middle">
+            <a
+              v-if="modelValue.length > 0"
+              @click.prevent="toggleSelectAll"
+              href="#"
+            >
+              <i
+                v-if="isAllSelected()"
+                class="bi-check-square-fill text-primary"
+              ></i>
+              <i v-else class="bi-square text-muted"></i>
+            </a>
+          </th>
           <th></th>
-          <slot name="head" :items="modelValue"></slot>
+          <slot
+            name="head"
+            :items="modelValue"
+            :selected-items="selected"
+          ></slot>
           <th width="30"></th>
         </tr>
       </thead>
@@ -17,6 +34,15 @@
       >
         <template #item="{ element: item, index }">
           <tr>
+            <td v-if="selectable" class="text-center align-middle">
+              <a @click.prevent="toggleSelect(item)" href="#">
+                <i
+                  v-if="isSelected(item)"
+                  class="bi-check-square-fill text-primary"
+                ></i>
+                <i v-else class="bi-square text-muted"></i>
+              </a>
+            </td>
             <td class="handle text-center px-0">
               <i
                 class="bi-grip-vertical text-muted"
@@ -55,7 +81,7 @@
                     <i class="fa fa-copy"></i> Copy
                   </a>
                   <a
-                    @click.prevent="removeItemAtIndex(index)"
+                    @click.prevent="removeItemAtIndex(index, item)"
                     href="#"
                     class="dropdown-item"
                   >
@@ -95,6 +121,8 @@
 import _ from "lodash";
 import VueDraggable from "vuedraggable";
 import { v4 as uuidv4 } from "uuid";
+import { ref } from "@vue/reactivity";
+import BatchSelector from "vio/helpers/BatchSelector";
 
 export default {
   name: "ArrayTableSortable",
@@ -120,18 +148,32 @@ export default {
       type: String,
       default: "",
     },
+    selectable: {
+      type: Boolean,
+      default: false,
+    },
   },
   components: {
     VueDraggable,
   },
   emits: ["add-item", "copy-item", "remove-items"],
   setup(props) {
-    return {};
+    const batch = new BatchSelector();
+    const selected = ref([]);
+
+    return {
+      batch,
+      selected,
+    };
   },
   mounted() {
     this.addKeys();
   },
-  computed() {},
+  computed: {
+    selectedItems() {
+      return this.selected;
+    },
+  },
   methods: {
     addKeys() {
       _.forEach(this.modelValue, (item, index) => {
@@ -157,9 +199,74 @@ export default {
       this.modelValue.push(copy);
       this.$emit("copy-item", copy);
     },
-    removeItemAtIndex(index) {
+    removeItemAtIndex(index, item) {
       let removed = this.modelValue.splice(index, 1);
+      this.unselectItem(item);
       this.$emit("remove-items", removed);
+    },
+    removeItem(itemToRemove) {
+      console.log("removeItem", itemToRemove);
+      _.remove(this.modelValue, (item) => {
+        return itemToRemove._uuid == item._uuid;
+      });
+
+      this.unselectItem(itemToRemove);
+    },
+    removeItems(items) {
+      let uuids = _.map(items, (item) => item._uuid);
+      _.remove(this.modelValue, (item) => {
+        let match = _.includes(uuids, item._uuid);
+        if (match) {
+          this.unselectItem(item);
+        }
+        return match;
+      });
+    },
+    selectItem(item) {
+      if (this.isSelected(item)) {
+        return;
+      }
+      this.selected.push(item);
+    },
+    unselectItem(item) {
+      _.remove(this.selected, (selectedItem) => {
+        return item._uuid == selectedItem._uuid;
+      });
+    },
+    isSelected(item) {
+      let index = _.findIndex(this.selected, (selectedItem) => {
+        return item._uuid == selectedItem._uuid;
+      });
+
+      return index > -1;
+    },
+    isAllSelected() {
+      let N = this.modelValue.length;
+      return N > 0 && this.selected.length >= N;
+    },
+    toggleSelectAll() {
+      if (this.isAllSelected()) {
+        return this.unselectAll();
+      }
+
+      return this.selectAll();
+    },
+    toggleSelect(item) {
+      if (this.isSelected(item)) {
+        this.unselectItem(item);
+      } else {
+        this.selectItem(item);
+      }
+    },
+    unselectAll() {
+      this.selected = [];
+    },
+    selectAll() {
+      _.forEach(this.modelValue, (item) => {
+        if (!this.isSelected(item)) {
+          this.selectItem(item);
+        }
+      });
     },
   },
 };
@@ -175,5 +282,9 @@ table td.handle {
   width: 15px !important;
   padding-left: 0 !important;
   padding-right: 0 !important;
+}
+
+table td.narrow {
+  width: 15px !important;
 }
 </style>

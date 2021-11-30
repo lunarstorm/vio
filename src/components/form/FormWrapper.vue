83a6811702567d03ef3b9f5c1bd71d25724bf39e
Jasper Tey <jasper.tey@gmail.com>
2021-11-30 17:44:04 -0500

    WIP.

---
+++ src/components/form/FormWrapper.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <slot name="head"></slot>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { provide } from "vue";
+import Form from "vio/helpers/Form";
+
+export default {
+  name: "FormWrapper",
+  inheritAttrs: true,
+  components: {},
+  props: {
+    form: [Form],
+  },
+  setup(props) {
+    provide("form", props.form);
+
+    return {};
+  },
+  computed: {},
+  methods: {},
+  mounted() {},
+  unmounted() {},
+};
+</script>
+
+<style scoped>
+</style>

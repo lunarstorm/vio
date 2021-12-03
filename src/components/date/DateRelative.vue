4b7455f1b2145543e1734c87edb7442bc02ecdea
Jasper Tey <jasper.tey@gmail.com>
2021-12-02 19:15:14 -0500

    Cleaner date component for relative time, the start of more formal Date* components.

---
+++ src/components/date/DateRelative.vue
@@ -0,0 +1,36 @@
+<template>
+  <span ref="el" :title="title">{{ dateFormatted }}</span>
+</template>
+
+<script>
+import { DateTime } from "luxon";
+import Date from "vio/helpers/Date";
+
+export default {
+  name: "DateRelative",
+  props: {
+    date: [String, Number],
+  },
+  setup(props) {
+    const dt = Date.parse(props.date);
+
+    return {
+      dt,
+    };
+  },
+  mounted() {},
+  updated() {},
+  methods: {},
+  computed: {
+    dateFormatted() {
+      return this.dt.toRelative();
+    },
+    title() {
+      return this.dt.toLocaleString(DateTime.DATETIME_FULL);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>

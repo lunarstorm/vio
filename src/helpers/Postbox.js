92c0d2b7121037c6cd9a68ffc350b6f9fd5820c8
Jasper Tey <jasper.tey@gmail.com>
2021-12-07 10:37:12 -0500

    ES Linter auto format.

--- src/helpers/Postbox.js
+++ src/helpers/Postbox.js
@@ -1,141 +1,141 @@
-import {reactive, watch, getCurrentInstance, onUnmounted} from 'vue';
+import { reactive, watch, getCurrentInstance, onUnmounted } from 'vue';
 import _ from 'lodash';
 
 class Postbox {
-	constructor(defaults) {
-		this.data = reactive({
-			store: {},
-			handlers: {},
-			collections: {},
-			messages: [],
-		});
+    constructor(defaults) {
+        this.data = reactive({
+            store: {},
+            handlers: {},
+            collections: {},
+            messages: [],
+        });
 
-		this.init();
-	}
+        this.init();
+    }
 
-	post(topic, data){
-		//console.log("Postbox.post", topic, data);
-		this.data.messages.push({
-			topic: topic,
-			data: data
-		});
-	}
+    post(topic, data) {
+        //console.log("Postbox.post", topic, data);
+        this.data.messages.push({
+            topic: topic,
+            data: data
+        });
+    }
 
-	commit(topic, data) {
-		//console.log("Postbox.commit", topic, data);
-		this.data.messages.push({
-			topic: `${topic}.commit`,
-			data: data
-		});
-	}
+    commit(topic, data) {
+        //console.log("Postbox.commit", topic, data);
+        this.data.messages.push({
+            topic: `${topic}.commit`,
+            data: data
+        });
+    }
 
-	syncWithArray(collection, topic) {
-		if (!_.has(this.data.collections, topic)) {
-			_.set(this.data.collections, topic, []);
-		}
+    syncWithArray(collection, topic) {
+        if (!_.has(this.data.collections, topic)) {
+            _.set(this.data.collections, topic, []);
+        }
 
-		let pool = this.getCollections(topic);
-		pool.push(collection);
-	}
+        let pool = this.getCollections(topic);
+        pool.push(collection);
+    }
 
-	getCollections(topic) {
-		return _.get(this.data.collections, topic, []);
-	}
+    getCollections(topic) {
+        return _.get(this.data.collections, topic, []);
+    }
 
-	on(topic, callback){
-		let topics = _.castArray(topic);
+    on(topic, callback) {
+        let topics = _.castArray(topic);
 
-		_.forEach(topics, topic => {
-			if (!_.has(this.data.handlers, topic)) {
-				_.set(this.data.handlers, topic, []);
-			}
+        _.forEach(topics, topic => {
+            if (!_.has(this.data.handlers, topic)) {
+                _.set(this.data.handlers, topic, []);
+            }
 
-			let handlers = this.getHandlers(topic);
-			handlers.push(callback);
+            let handlers = this.getHandlers(topic);
+            handlers.push(callback);
 
-			let component = getCurrentInstance();
-			if (component) {
-				onUnmounted(() => {
-					//console.log('Postbox auto-unmounting?');
-					this.off(topic, callback);
-				})
-			}
-		});
-	}
+            let component = getCurrentInstance();
+            if (component) {
+                onUnmounted(() => {
+                    //console.log('Postbox auto-unmounting?');
+                    this.off(topic, callback);
+                })
+            }
+        });
+    }
 
-	off(topic, callback) {
-		let topics = _.castArray(topic);
+    off(topic, callback) {
+        let topics = _.castArray(topic);
 
-		_.forEach(topics, topic => {
-			let handlers = this.getHandlers(topic);
-			_.remove(handlers, handler => {
-				return handler === callback;
-			});
-		});
-	}
+        _.forEach(topics, topic => {
+            let handlers = this.getHandlers(topic);
+            _.remove(handlers, handler => {
+                return handler === callback;
+            });
+        });
+    }
 
-	getHandlers(topic){
-		return _.get(this.data.handlers, topic, []);
-	}
+    getHandlers(topic) {
+        return _.get(this.data.handlers, topic, []);
+    }
 
-	handleMessage(message){
-		let handlers = this.getHandlers(message.topic);
-		_.forEach(handlers, handler => {
-			if(typeof handler == "function"){
-				handler(message.data, message.topic);
-			}
-		});
+    handleMessage(message) {
+        let handlers = this.getHandlers(message.topic);
+        _.forEach(handlers, handler => {
+            if (typeof handler == "function") {
+                handler(message.data, message.topic);
+            }
+        });
 
-		if(_.endsWith(message.topic, '.commit')){
-			let parts = _.toPath(message.topic);
+        if (_.endsWith(message.topic, '.commit')) {
+            let parts = _.toPath(message.topic);
 
-			//console.log('commit detected', parts);
+            //console.log('commit detected', parts);
 
-			if(parts.length > 0){
-				let collections = this.getCollections(parts[0])
+            if (parts.length > 0) {
+                let collections = this.getCollections(parts[0])
 
-				//console.log('collections', collections);
+                //console.log('collections', collections);
 
-				if(message.data && message.data.id){
-					let key = message.data.id;
+                if (message.data && message.data.id) {
+                    let key = message.data.id;
 
-					if (!key) {
-						return;
-					}
+                    if (!key) {
+                        return;
+                    }
 
-					_.forEach(collections, collection => {
-						//console.log('Searching collection for key', key);
+                    _.forEach(collections, collection => {
+                        //console.log('Searching collection for key', key);
 
-						let match = _.find(collection, item => {
-							return item.id == key;
-						});
+                        let match = _.find(collection, item => {
+                            return item.id == key;
+                        });
 
-						if (match) {
-							Object.assign(match, message.data);
-						}
-						else{
-							collection.unshift(message.data);
-						}
-					});
-				}
-			}
-		}
-	}
+                        if (match) {
+                            Object.assign(match, message.data);
+                        }
+                        else {
+                            collection.unshift(message.data);
+                        }
+                    });
+                }
+            }
+        }
+    }
 
-	init(){
-		watch(
-				() => this.data.messages,
-				(messages, prevMessages) => {
-					let message = messages.pop();
-					if(message){
-						//console.log("message received", message);
-						//console.log('handlers', this.data.handlers);
-						this.handleMessage(message);
-					}
-				},
-				{deep: true}
-		);
-	}
+    init() {
+        watch(
+            () => this.data.messages,
+            (messages, prevMessages) => {
+                let message = messages.pop();
+                if (message) {
+                    //console.log("message received", message);
+                    //console.log('handlers', this.data.handlers);
+                    this.handleMessage(message);
+                }
+            },
+            { deep: true }
+        );
+    }
 }
 
 const instance = new Postbox();

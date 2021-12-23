6f0fe6b85993679a33e84aefc5bb3580fac9bca1
Jasper Tey <jasper.tey@gmail.com>
2021-12-23 16:24:56 -0500

    ESLint autoformat.

--- src/components/form/FormError.vue
+++ src/components/form/FormError.vue
@@ -1,47 +1,40 @@
 <template>
-	<div
-	  v-if="errorText"
-	  class="form-text text-danger"
-	>
-		{{ errorText }}
-	</div>
+  <div v-if="errorText" class="form-text text-danger">
+    {{ errorText }}
+  </div>
 </template>
 
 <script>
-import _ from 'lodash';
-import {toRefs} from "vue";
+import _ from "lodash";
+import { toRefs } from "vue";
 
 export default {
-	name: "FormError",
-	inheritAttrs: true,
-	components: {},
-	props: {
-		modelValue: [Array, String, Number],
-	},
-	setup(props) {
-		const pRefs = toRefs(props);
-		return {
-			error: pRefs.modelValue
-		};
-	},
-	computed: {
-		errorText(){
-			if(_.isArray(this.error)){
-				return this.error.join(', ');
-			}
+  name: "FormError",
+  inheritAttrs: true,
+  components: {},
+  props: {
+    modelValue: [Array, String, Number],
+  },
+  setup(props) {
+    const pRefs = toRefs(props);
+    return {
+      error: pRefs.modelValue,
+    };
+  },
+  computed: {
+    errorText() {
+      if (_.isArray(this.error)) {
+        return this.error.join(", ");
+      }
 
-			return this.error;
-		}
-	},
-	methods: {},
-	mounted() {
-
-	},
-	unmounted() {
-	}
-}
+      return this.error;
+    },
+  },
+  methods: {},
+  mounted() {},
+  unmounted() {},
+};
 </script>
 
 <style scoped>
-
 </style>

7e48debccede68f5bb58c58c8884be644ea36cc9
Jasper Tey <jasper.tey@gmail.com>
2022-03-09 12:33:14 -0500

    Formatting.

--- src/vendor/inertia/BreadCrumbs.vue
+++ src/vendor/inertia/BreadCrumbs.vue
@@ -1,52 +1,46 @@
 <template>
-	<ul class="breadcrumb m-2">
-		<li
-		  v-for="crumb in crumbs"
-		  :key="crumb.title"
-		  class="breadcrumb-item"
-		>
-			<inertia-link
-			  :href="crumb.url"
-			>
-				{{ crumb.title }}
-			</inertia-link>
-		</li>
-	</ul>
+  <ul class="breadcrumb m-2">
+    <li v-for="crumb in crumbs" :key="crumb.title" class="breadcrumb-item">
+      <inertia-link :href="crumb.url">
+        {{ crumb.title }}
+      </inertia-link>
+    </li>
+  </ul>
 </template>
 
 <script>
-import {ref, watchEffect} from 'vue';
+import { ref, watchEffect } from "vue";
 
 export default {
-	name: "BreadCrumbs",
-	props: {
-		home: Boolean,
-		baseTitle: {
-			type: String,
-			default: ''
-		},
-		crumbs: Array
-	},
-	components: {},
-	setup(props) {
-		const baseTitle = document.title;
+  name: "BreadCrumbs",
+  props: {
+    home: Boolean,
+    baseTitle: {
+      type: String,
+      default: "",
+    },
+    crumbs: Array,
+  },
+  components: {},
+  setup(props) {
+    const baseTitle = document.title;
 
-		return {
-			baseTitle: ref(props.baseTitle),
-			showHome: ref(props.home),
-		}
-	},
-	beforeMount(){
-		watchEffect(() => {
-			//document.title = this.title;
-		});
-	},
-	mounted() {
-		//console.log(this.crumbs);
-	},
-	computed: {
-		title(){
-			/*let pathReversed = this.crumbs.slice().reverse();
+    return {
+      baseTitle: ref(props.baseTitle),
+      showHome: ref(props.home),
+    };
+  },
+  beforeMount() {
+    watchEffect(() => {
+      //document.title = this.title;
+    });
+  },
+  mounted() {
+    //console.log(this.crumbs);
+  },
+  computed: {
+    title() {
+      /*let pathReversed = this.crumbs.slice().reverse();
 			let tokens = pathReversed.map(item => item.text);
 
 			if(this.baseTitle){
@@ -55,12 +49,11 @@ export default {
 
 			let title = tokens.join(' | ');
 			return title;*/
-		}
-	},
-	methods: {}
-}
+    },
+  },
+  methods: {},
+};
 </script>
 
 <style scoped>
-
 </style>

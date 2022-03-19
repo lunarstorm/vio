e8df2ba83497611246c3296a6f103fe2e85faffc
Jasper Tey <jasper.tey@gmail.com>
2022-03-19 05:12:46 -0400

    First draft DataGet component adapted from Data Provider pattern.

---
+++ src/components/data/DataGet.vue
@@ -0,0 +1,34 @@
+<template>
+  <slot :response="res" :loading="loading"></slot>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  props: {
+    url: String,
+  },
+  components: {},
+  setup(props) {
+    const res = ref("");
+    const loading = ref(true);
+
+    axios
+      .get(props.url)
+      .then((response) => response.data)
+      .then((response) => {
+        res.value = response;
+        loading.value = false;
+      });
+
+    return {
+      res,
+      loading,
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>

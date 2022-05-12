595af2329e30d01cd10f626575b981c5cfe944d3
Jasper Tey <jasper.tey@gmail.com>
2022-05-12 00:10:21 -0400

    Beginnings of form index.js to support multi-import syntax.

---
+++ src/components/form/index.js
@@ -0,0 +1,17 @@
+import InputText from './InputText';
+import InputTextarea from './InputTextarea';
+import InputGroup from './InputGroup';
+import InputDate from './InputDate';
+import OptionsMulti from './OptionsMulti';
+import FormGroup from './FormGroup';
+import ButtonSubmit from './ButtonSubmit';
+
+export {
+    InputText,
+    InputTextarea,
+    InputGroup,
+    InputDate,
+    OptionsMulti,
+    FormGroup,
+    ButtonSubmit,
+};

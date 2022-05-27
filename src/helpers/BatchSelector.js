1b7c86d9032765fe752b44f73e79f6285661066b
Jasper Tey <jasper.tey@gmail.com>
2022-05-26 21:06:28 -0400

    Cleanup and support specifying a custom key via "keyName" argument.

--- src/helpers/BatchSelector.js
+++ src/helpers/BatchSelector.js
@@ -2,122 +2,124 @@ import {computed, reactive, toRaw, unref} from 'vue';
 import _ from 'lodash';
 
 class BatchSelector {
-	constructor(args) {
-		let state = reactive({
-			selectedMap: {},
-			selectedValues: [],
-			items: [],
-			...args
-		});
-		this.state = state;
+    constructor(args) {
+        let state = reactive({
+            selectedMap: {},
+            selectedValues: [],
+            items: [],
+            keyName: 'id',
+            ...args,
+        });
+        this.state = state;
 
-		this.nSelected = computed(function () {
-			return state.selectedValues.value.length;
-		});
+        this.nSelected = computed(function () {
+            return state.selectedValues.value.length;
+        });
 
-		this.items = computed(() => state.items);
-		this.selectedMap = computed(() => state.selectedMap);
-		this.selectedValues = computed(() => state.selectedValues);
-	}
+        this.items = computed(() => state.items);
+        this.selectedMap = computed(() => state.selectedMap);
+        this.selectedValues = computed(() => state.selectedValues);
+    }
 
-	bindWithItems(items){
-		//Object.assign(this.state.items, items)
-		this.clear();
-		this.state.items = items;
-	}
+    bindWithItems(items){
+        //Object.assign(this.state.items, items)
+        this.clear();
+        this.state.items = items;
+    }
 
-	isAllSelected(){
-		return this.count() > 0 && (this.count() === this.state.items.length);
-	}
+    isAllSelected(){
+        return this.count() > 0 && (this.count() === this.state.items.length);
+    }
 
-	isSomeSelected() {
-		return this.count() > 0;
-	}
+    isSomeSelected() {
+        return this.count() > 0;
+    }
 
-	isNoneSelected(){
-		return this.count() === 0;
-	}
+    isNoneSelected(){
+        return this.count() === 0;
+    }
 
-	isSelected(key) {
-		//let haystack = unref(this.state.selectedValues);
-		//let pos = _.findIndex(haystack, (item) => item == key);
+    isSelected(key) {
+        //let haystack = unref(this.state.selectedValues);
+        //let pos = _.findIndex(haystack, (item) => item == key);
 
-		return !!this.state.selectedMap[key];
-		//return pos > -1;
-	}
+        return !!this.state.selectedMap[key];
+        //return pos > -1;
+    }
 
-	select(key) {
-		if (!this.isSelected(key)) {
-			this.state.selectedValues.push(key);
-			this.state.selectedMap[key] = true;
-		}
-		return false;
-	}
+    select(key) {
+        if (!this.isSelected(key)) {
+            this.state.selectedValues.push(key);
+            this.state.selectedMap[key] = true;
+        }
+        return false;
+    }
 
-	unselect(key) {
-		_.remove(this.state.selectedValues, (item) => item == key);
-		this.state.selectedMap[key] = false;
-		return false;
-	}
+    unselect(key) {
+        _.remove(this.state.selectedValues, (item) => item == key);
+        this.state.selectedMap[key] = false;
+        return false;
+    }
 
-	selectAll(){
-		_.forEach(this.state.items, (item) => {
-			this.select(item.id);
-		})
+    selectAll(){
+        _.forEach(this.state.items, (item) => {
+            let key = item[this.state.keyName];
+            this.select(key);
+        });
 
-		return true;
-	}
+        return true;
+    }
 
-	selectAllToggle() {
-		if(this.isAllSelected()){
-			return this.selectNone();
-		}
+    selectAllToggle() {
+        if(this.isAllSelected()){
+            return this.selectNone();
+        }
 
-		return this.selectAll();
-	}
+        return this.selectAll();
+    }
 
-	selectNone() {
-		return this.clear();
-	}
+    selectNone() {
+        return this.clear();
+    }
 
-	clear() {
-		let self = this;
-		this.state.selectedValues = [];
-		this.state.selectedMap = {};
+    clear() {
+        let self = this;
+        this.state.selectedValues = [];
+        this.state.selectedMap = {};
 
-		/*_.forEach(this.state.selectedMap, function(value, key){
+        /*_.forEach(this.state.selectedMap, function(value, key){
 			console.log('unselect', key);
 			self.unselect(key);
 		});*/
 
-		//console.log(self.selectedMap.value, self.selectedValues.value);
+        //console.log(self.selectedMap.value, self.selectedValues.value);
 
-		return false;
-	}
+        return false;
+    }
 
-	reset() {
-		this.clear();
-	}
+    reset() {
+        this.clear();
+    }
 
-	count() {
-		return this.selectedValues.value.length;
-	}
+    count() {
+        return this.selectedValues.value.length;
+    }
 
-	toggle(key) {
-		if (this.isSelected(key)) {
-			this.unselect(key);
-		} else {
-			this.select(key);
-		}
-		//console.log('Batch.selectedValues', this.selectedValues.value.length);
-		return false;
-	}
+    toggle(key) {
+        if (this.isSelected(key)) {
+            this.unselect(key);
+        } else {
+            this.select(key);
+        }
+        //console.log('Batch.selectedValues', this.selectedValues.value.length);
+        return false;
+    }
 
-	getValues() {
-		return toRaw(unref(this.state.selectedValues));
-	}
+    getValues() {
+        return toRaw(unref(this.state.selectedValues));
+    }
 
-	/*stateKeys() {
+    /*stateKeys() {
 		let obj = this.state.selectedMap;
 		let keys = Object.keys(obj);
 		let filtered = keys.filter(function (key) {

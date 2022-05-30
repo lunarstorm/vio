a0b5e8f8ac8f4178f8293383d646a806ae270b1a
Jasper Tey <jasper.tey@gmail.com>
2022-05-29 23:00:28 -0400

    Hide dropdown if items when items slot isn't present.

--- src/components/menu/DotMenu.vue
+++ src/components/menu/DotMenu.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="btn-group">
     <slot name="before" />
-    <button
-      type="button"
-      class="btn btn-light px-2"
-      data-toggle="dropdown"
-      data-boundary="viewport"
-      aria-haspopup="true"
-      aria-expanded="false"
-    >
-      <i class="bi bi-three-dots font-size-130" />
-    </button>
-    <div class="dropdown-menu dropdown-menu-right">
-      <slot name="items" />
-    </div>
+    <template v-if="!!$slots.items">
+      <button
+        type="button"
+        class="btn btn-light px-2"
+        data-toggle="dropdown"
+        data-boundary="viewport"
+        aria-haspopup="true"
+        aria-expanded="false"
+      >
+        <i class="bi bi-three-dots font-size-130" />
+      </button>
+      <div class="dropdown-menu dropdown-menu-right">
+        <slot name="items" />
+      </div>
+    </template>
   </div>
 </template>
 

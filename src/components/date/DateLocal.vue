4f31e1d512627d3e251bba07f1ac93718553066e
Jasper Tey <jasper.tey@gmail.com>
2022-08-16 12:37:55 -0400

    DateLocal

---
+++ src/components/date/DateLocal.vue
@@ -0,0 +1,27 @@
+<template>
+  <span ref="el" :title="dateFormatted">{{ dateFormatted }}</span>
+</template>
+
+<script>
+import { DateTime } from 'luxon';
+import Date from 'vio/helpers/Date';
+
+export default {
+    name: 'DateLocal',
+    props: {
+        date: [String, Number],
+    },
+    setup(props) {
+        const dt = Date.parse(props.date);
+
+        return {
+            dt,
+        };
+    },
+    computed: {
+        dateFormatted() {
+            return this.dt.toLocaleString(DateTime.DATETIME_FULL);
+        },
+    },
+};
+</script>

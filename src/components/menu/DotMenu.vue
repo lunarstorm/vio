7f8e87163574cfa716d177ef59e66e2da643d524
Jasper Tey <jasper.tey@gmail.com>
2022-10-02 18:04:59 -0400

    Allow controling the alignment of the menu (left or right).

--- src/components/menu/DotMenu.vue
+++ src/components/menu/DotMenu.vue
@@ -12,7 +12,7 @@
       >
         <i class="bi bi-three-dots font-size-130" />
       </button>
-      <div class="dropdown-menu dropdown-menu-right">
+      <div class="dropdown-menu" :class="dropdownMenuClasses">
         <slot name="items" />
       </div>
     </template>
@@ -23,11 +23,26 @@
 export default {
     name: 'DotMenu',
     components: {},
-    props: {},
+    props: {
+        align: {
+            type: String,
+            default: 'right',
+        },
+    },
     setup(props) {
         return {};
     },
-    computed: {},
+    computed: {
+        dropdownMenuClasses(){
+            let clases = [];
+
+            if(this.align == 'right'){
+                clases.push('dropdown-menu-right');
+            }
+
+            return clases;
+        },
+    },
     mounted() { },
 };
 </script>
